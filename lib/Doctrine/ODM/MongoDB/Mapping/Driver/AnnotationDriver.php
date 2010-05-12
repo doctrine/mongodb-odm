7cfc0849e791816ae1b492ae3ae85620ad1a44ae
Bulat (Hacker) Shakirzyanov <mallluhuct@gmail.com>
2010-05-12 17:12:01 -0400

    added missing @Hash annotation

--- lib/Doctrine/ODM/MongoDB/Mapping/Driver/AnnotationDriver.php
+++ lib/Doctrine/ODM/MongoDB/Mapping/Driver/AnnotationDriver.php
@@ -136,7 +136,7 @@ class AnnotationDriver implements Driver
             $types = array(
                 'Id', 'File', 'Field', 'String', 'Boolean', 'Int', 'Float', 'Date',
                 'Key', 'Bin', 'BinFunc', 'BinUUID', 'BinMD5', 'BinCustom', 'EmbedOne',
-                'EmbedMany', 'ReferenceOne', 'ReferenceMany', 'Timestamp'
+                'EmbedMany', 'ReferenceOne', 'ReferenceMany', 'Timestamp', 'Hash'
             );
             foreach ($types as $type) {
                 if ($fieldAnnot = $this->_reader->getPropertyAnnotation($property, 'Doctrine\ODM\MongoDB\Mapping\\' . $type)) {
