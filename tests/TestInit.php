cfb18b761a82249d16282b52089221818dedb561
Jonathan H. Wage <jonwage@gmail.com>
2010-05-12 10:36:28 -0400

    Fixing hardcoded path

--- tests/TestInit.php
+++ tests/TestInit.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once 'PHPUnit/Framework.php';
-require_once '/Users/jwage/Sites/doctrine2git/lib/Doctrine/Common/ClassLoader.php';
+require_once $_SERVER['DOCTRINE2_DIR'] . '/Doctrine/Common/ClassLoader.php';
 require_once 'BaseTest.php';
 
 use Doctrine\Common\ClassLoader;
@@ -9,7 +9,10 @@ use Doctrine\Common\ClassLoader;
 $classLoader = new ClassLoader('Doctrine\ODM', __DIR__ . '/../lib');
 $classLoader->register();
 
-$classLoader = new ClassLoader('Doctrine', '/Users/jwage/Sites/doctrine2git/lib');
+$classLoader = new ClassLoader('Doctrine', $_SERVER['DOCTRINE2_DIR']);
+$classLoader->register();
+
+$classLoader = new ClassLoader('Symfony', $_SERVER['DOCTRINE2_DIR'] . '/vendor');
 $classLoader->register();
 
 $classLoader = new ClassLoader('Documents', __DIR__);
