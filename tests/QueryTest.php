11384b947ea3f4256e172e576f531e2234784f1b
Jonathan H. Wage <jonwage@gmail.com>
2010-05-18 16:26:41 -0400

    Adding blank test for Query

---
+++ tests/QueryTest.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once 'TestInit.php';
+
+use Documents\Account,
+    Documents\Address,
+    Documents\Group,
+    Documents\Phonenumber,
+    Documents\Profile,
+    Documents\File,
+    Documents\User;
+
+class QueryTest extends BaseTest
+{
+    public function testBasicQuery()
+    {
+        $user = new User();
+        $user->setUsername('boo');
+        $this->dm->persist($user);
+        $this->dm->flush();
+
+        $query = $this->dm->createQuery('Documents\User')
+            ->where('$where', "function() { return this.username == 'boo' }");
+        $user = $query->getSingleResult();
+        $this->assertEquals('boo', $user->getUsername());
+    }
+}
