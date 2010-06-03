14f6684629ac9650d384f54cff58346c626e0214
Jonathan H. Wage <jonwage@gmail.com>
2010-06-03 13:05:03 -0400

    Removing example folder. Examples can be gotten from the tests.

--- example/performance.mysql.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$pdo = new PDO('mysql:host=localhost;dbname=performance', 'root', null);
-$stmt = $pdo->prepare('INSERT INTO user (username, password) VALUES (?, ?)');
-$stmt->execute(array('user', 'password'));
