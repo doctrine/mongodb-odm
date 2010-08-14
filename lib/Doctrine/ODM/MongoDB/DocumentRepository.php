611db8c8e6ff46c984f66b6db76c49a5de6361a2
ornicar <thibault.duplessis@gmail.com>
2010-08-14 18:27:00 +0200

    Fix PHPdoc in DocumentRepository

--- lib/Doctrine/ODM/MongoDB/DocumentRepository.php
+++ lib/Doctrine/ODM/MongoDB/DocumentRepository.php
@@ -63,9 +63,9 @@ class DocumentRepository
     }
 
     /**
-     * Create a new QueryBuilder instance that is prepopulated for this document name
+     * Create a new Query instance that is prepopulated for this document name
      *
-     * @return QueryBuilder $qb
+     * @return Query $qb
      */
     public function createQuery()
     {
