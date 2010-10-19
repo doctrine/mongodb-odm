005ef37e7dcd8f59fd7665f52aebc96c74988c77
ornicar <thibault.duplessis@gmail.com>
2010-10-19 14:13:08 +0200

    Add tests showing that PreRemove and PostRemove lifecycle callbacks are not called on embedded(many) documents

--- tests/Documents/Functional/EmbeddedTestLevel1.php
+++ tests/Documents/Functional/EmbeddedTestLevel1.php
@@ -2,11 +2,29 @@
 
 namespace Documents\Functional;
 
-/** @EmbeddedDocument */
+/**
+ * @EmbeddedDocument
+ * @HasLifecycleCallbacks
+ */
 class EmbeddedTestLevel1
 {
     /** @String */
     public $name;
     /** @EmbedMany(targetDocument="EmbeddedTestLevel2") */
     public $level2 = array();
+
+    public $preRemove = false;
+    public $postRemove = false;
+
+    /** @PreRemove */
+    public function onPreRemove()
+    {
+        $this->preRemove = true;
+    }
+
+    /** @PostRemove */
+    public function onPostRemove()
+    {
+        $this->postRemove = true;
+    }
 }
