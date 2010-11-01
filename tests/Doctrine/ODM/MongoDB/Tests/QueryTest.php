c899f65ec25611882a96e1e86d51864d09512adf
Kris Wallsmith <kris.wallsmith@gmail.com>
2010-11-01 06:35:43 -0700

    [MODM-94] cleaned up test

--- tests/Doctrine/ODM/MongoDB/Tests/QueryTest.php
+++ tests/Doctrine/ODM/MongoDB/Tests/QueryTest.php
@@ -11,6 +11,8 @@ class QueryTest extends \PHPUnit_Framework_TestCase
     public function testThatOrAcceptsAnotherQuery()
     {
         $class = 'Person';
+        $expression1 = array('first_name' => 'Kris');
+        $expression2 = array('first_name' => 'Chris');
 
         $dm = $this->getMockBuilder('Doctrine\\ODM\\MongoDB\\DocumentManager')
             ->disableOriginalConstructor()
@@ -31,17 +33,14 @@ class QueryTest extends \PHPUnit_Framework_TestCase
         $dm
             ->expects($this->once())
             ->method('find')
-            ->with('Person', array(
-                '$or' => array(
-                    array('first_name' => 'Kris'),
-                    array('first_name' => 'Chris'),
-                ),
+            ->with($class, array(
+                '$or' => array($expression1, $expression2),
             ))
             ->will($this->returnValue($cursor));
 
         $query = new Query($dm, $class);
-        $query->addOr(array('first_name' => 'Kris'));
-        $query->addOr(array('first_name' => 'Chris'));
+        $query->addOr($expression1);
+        $query->addOr($expression2);
 
         $this->assertSame($cursor, $query->execute());
     }
