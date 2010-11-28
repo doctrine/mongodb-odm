6d534bdc91f4584be5b2c18d4d44fcbd0adc5550
Fabien Potencier <fabien.potencier@gmail.com>
2010-11-28 10:07:27 +0100

    Update Symfony\Component\Console to latest version

--- lib/vendor/Symfony/Component/Console/Command/Command.php
+++ lib/vendor/Symfony/Component/Console/Command/Command.php
@@ -21,7 +21,7 @@ use Symfony\Component\Console\Application;
 /**
  * Base class for all commands.
  *
- * @author     Fabien Potencier <fabien.potencier@symfony-project.com>
+ * @author Fabien Potencier <fabien.potencier@symfony-project.com>
  */
 class Command
 {
@@ -236,9 +236,9 @@ class Command
      *
      * @param string  $name        The option name
      * @param string  $shortcut    The shortcut (can be null)
-     * @param integer $mode        The option mode: self::PARAMETER_REQUIRED, self::PARAMETER_NONE or self::PARAMETER_OPTIONAL
+     * @param integer $mode        The option mode: One of the InputOption::VALUE_* constants
      * @param string  $description A description text
-     * @param mixed   $default     The default value (must be null for self::PARAMETER_REQUIRED or self::PARAMETER_NONE)
+     * @param mixed   $default     The default value (must be null for InputOption::VALUE_REQUIRED or self::VALUE_NONE)
      *
      * @return Command The current instance
      */
--- lib/vendor/Symfony/Component/Console/Command/ListCommand.php
+++ lib/vendor/Symfony/Component/Console/Command/ListCommand.php
@@ -21,7 +21,7 @@ use Symfony\Component\Console\Command\Command;
 /**
  * ListCommand displays the list of all available commands for the application.
  *
- * @author     Fabien Potencier <fabien.potencier@symfony-project.com>
+ * @author Fabien Potencier <fabien.potencier@symfony-project.com>
  */
 class ListCommand extends Command
 {
@@ -33,7 +33,7 @@ class ListCommand extends Command
         $this
             ->setDefinition(array(
                 new InputArgument('namespace', InputArgument::OPTIONAL, 'The namespace name'),
-                new InputOption('xml', null, InputOption::PARAMETER_NONE, 'To output help as XML'),
+                new InputOption('xml', null, InputOption::VALUE_NONE, 'To output help as XML'),
             ))
             ->setName('list')
             ->setDescription('Lists commands')
