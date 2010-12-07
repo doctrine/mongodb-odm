93c5b361c960f91093400da32a60916a466fc68d
Jonathan H. Wage <jonwage@gmail.com>
2010-12-07 00:18:04 -0600

    Adding hydrated data instead of trying to modify and unset fields in the original data.

--- lib/Doctrine/ODM/MongoDB/Hydrator/HydratorFactory.php
+++ lib/Doctrine/ODM/MongoDB/Hydrator/HydratorFactory.php
@@ -68,29 +68,15 @@ class HydratorFactory
         $code = '';
 
         foreach ($class->fieldMappings as $fieldName => $mapping) {
-            $unsetCode = null;
-            if ($mapping['name'] !== $mapping['fieldName']) {
-                $unsetCode = sprintf(<<<EOF
-
-        unset(\$data['%1\$s']);
-EOF
-                ,
-                    $mapping['name'],
-                    $mapping['fieldName']
-                );
-            }
-            $unsetCode = <<<EOF
-
-            \$data['$fieldName'] = \$return;
-EOF;
-
             if (isset($mapping['alsoLoadFields'])) {
                 foreach ($mapping['alsoLoadFields'] as $name) {
                     $code .= sprintf(<<<EOF
 
+        /** @AlsoLoad("$name") */
         if (isset(\$data['$name'])) {
             \$data['$fieldName'] = \$data['$name'];
         }
+
 EOF
                     );
                 }
@@ -99,39 +85,43 @@ EOF
             if ( ! isset($mapping['association'])) {
                 $code .= sprintf(<<<EOF
 
+        /** @Field(type="{$mapping['type']}") */
         if (isset(\$data['%1\$s'])) {
             \$value = \$data['%1\$s'];
             %3\$s
-            \$this->class->reflFields['$fieldName']->setValue(\$document, \$return);%4\$s
+            \$this->class->reflFields['%2\$s']->setValue(\$document, \$return);
+            \$hydratedData['%2\$s'] = \$return;
         }
 
 EOF
                 ,
                     $mapping['name'],
                     $mapping['fieldName'],
-                    Type::getType($mapping['type'])->closureToPHP(),
-                    $unsetCode
+                    Type::getType($mapping['type'])->closureToPHP()
                 );
             } elseif ($mapping['association'] === ClassMetadata::REFERENCE_ONE) {
                 $code .= sprintf(<<<EOF
+
+        /** @ReferenceOne */
         if (isset(\$data['%1\$s'])) {
             \$reference = \$data['%1\$s'];
             \$className = \$this->dm->getClassNameFromDiscriminatorValue(\$this->class->fieldMappings['%2\$s'], \$reference);
             \$targetMetadata = \$this->dm->getClassMetadata(\$className);
             \$id = \$targetMetadata->getPHPIdentifierValue(\$reference['\$id']);
             \$return = \$this->dm->getReference(\$className, \$id);
-            \$this->class->reflFields['$fieldName']->setValue(\$document, \$return);%3\$s
+            \$this->class->reflFields['$fieldName']->setValue(\$document, \$return);
+            \$hydratedData['%2\$s'] = \$return;
         }
-
 
 EOF
                 ,
                     $mapping['name'],
-                    $mapping['fieldName'],
-                    $unsetCode
+                    $mapping['fieldName']
                 );
             } elseif ($mapping['association'] === ClassMetadata::REFERENCE_MANY || $mapping['association'] === ClassMetadata::EMBED_MANY) {
                 $code .= sprintf(<<<EOF
+
+        /** @Many */
         \$mongoData = isset(\$data['%1\$s']) ? \$data['%1\$s'] : null;
         \$return = new \Doctrine\ODM\MongoDB\PersistentCollection(new \Doctrine\Common\Collections\ArrayCollection(), \$this->dm, \$this->dm->getUnitOfWork(), '$');
         \$return->setOwner(\$document, \$this->class->fieldMappings['%2\$s']);
@@ -139,17 +129,18 @@ EOF
         if (\$mongoData) {
             \$return->setMongoData(\$mongoData);
         }
-        \$this->class->reflFields['$fieldName']->setValue(\$document, \$return);%3\$s
-
+        \$this->class->reflFields['$fieldName']->setValue(\$document, \$return);
+        \$hydratedData['%2\$s'] = \$return;
 
 EOF
                 ,
                     $mapping['name'],
-                    $mapping['fieldName'],
-                    $unsetCode
+                    $mapping['fieldName']
                 );
             } elseif ($mapping['association'] === ClassMetadata::EMBED_ONE) {
                 $code .= sprintf(<<<EOF
+
+        /** @EmbedOne */
         if (isset(\$data['%1\$s'])) {
             \$embeddedDocument = \$data['%1\$s'];
             \$className = \$this->dm->getClassNameFromDiscriminatorValue(\$this->class->fieldMappings['%2\$s'], \$embeddedDocument);
@@ -160,15 +151,14 @@ EOF
             \$this->dm->getUnitOfWork()->registerManaged(\$return, null, \$embeddedData);
             \$this->dm->getUnitOfWork()->setParentAssociation(\$return, \$this->class->fieldMappings['%2\$s'], \$document, '%1\$s');
 
-            \$this->class->reflFields['$fieldName']->setValue(\$document, \$return);%3\$s
+            \$this->class->reflFields['$fieldName']->setValue(\$document, \$return);
+            \$hydratedData['%2\$s'] = \$return;
         }
-
 
 EOF
                 ,
                     $mapping['name'],
-                    $mapping['fieldName'],
-                    $unsetCode
+                    $mapping['fieldName']
                 );
             }
         }
@@ -200,7 +190,8 @@ class $hydratorClassName implements HydratorInterface
 
     public function hydrate(\$document, \$data)
     {
-%s        return \$data;
+        \$hydratedData = array();
+%s        return \$hydratedData;
     }
 }
 EOF
