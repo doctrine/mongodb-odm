7e2b360580019f6f5538f5b468962fd8194e575e
Kris Wallsmith <kris.wallsmith@gmail.com>
2010-12-17 17:17:10 -0800

    BC break: updated $qb->references() to work with ReferenceneOne fields, added $qb->includesReferenceTo() for ReferenceMany fields, abstracted DBRef array creation to DocumentManager for better reuse

--- tests/Doctrine/ODM/MongoDB/Tests/QueryTest.php
+++ tests/Doctrine/ODM/MongoDB/Tests/QueryTest.php
@@ -33,9 +33,71 @@ class QueryTest extends BaseTest
         $this->assertInstanceOf('Doctrine\MongoDB\Cursor', $users);
         $this->assertEquals(2, count($users));
     }
+
+    public function testReferences()
+    {
+        $kris = new Person('Kris');
+        $jon = new Person('Jon');
+
+        $this->dm->persist($kris);
+        $this->dm->persist($jon);
+        $this->dm->flush();
+
+        $kris->bestFriend = $jon;
+        $this->dm->flush();
+
+        $qb = $this->dm->createQueryBuilder(__NAMESPACE__.'\Person');
+        $qb->field('bestFriend')->references($jon);
+
+        $queryArray = $qb->getQueryArray();
+        $this->assertEquals(array(
+            'bestFriend.$ref' => 'people',
+            'bestFriend.$id' => new \MongoId($jon->id),
+            'bestFriend.$db' => 'doctrine_odm_tests',
+            'bestFriend._doctrine_class_name' => 'Doctrine\ODM\MongoDB\Tests\Person',
+        ), $queryArray);
+
+        $query = $qb->getQuery();
+
+        $this->assertEquals(1, $query->count());
+        $this->assertSame($kris, $query->getSingleResult());
+    }
+
+    public function testIncludesReferenceTo()
+    {
+        $kris = new Person('Kris');
+        $jon = new Person('Jon');
+
+        $this->dm->persist($kris);
+        $this->dm->persist($jon);
+        $this->dm->flush();
+
+        $jon->friends[] = $kris;
+        $this->dm->flush();
+
+        $qb = $this->dm->createQueryBuilder(__NAMESPACE__.'\Person');
+        $qb->field('friends')->includesReferenceTo($kris);
+
+        $queryArray = $qb->getQueryArray();
+        $this->assertEquals(array(
+            'friends' => array(
+                '$elemMatch' => array(
+                    '$ref' => 'people',
+                    '$id' => new \MongoId($kris->id),
+                    '$db' => 'doctrine_odm_tests',
+                    '_doctrine_class_name' => 'Doctrine\ODM\MongoDB\Tests\Person',
+                ),
+            ),
+        ), $queryArray);
+
+        $query = $qb->getQuery();
+
+        $this->assertEquals(1, $query->count());
+        $this->assertSame($jon, $query->getSingleResult());
+    }
 }
 
-/** @Document */
+/** @Document(collection="people") */
 class Person
 {
     /** @Id */
@@ -43,6 +105,12 @@ class Person
 
     /** @String */
     public $firstName;
+
+    /** @ReferenceOne */
+    public $bestFriend;
+
+    /** @ReferenceMany */
+    public $friends = array();
 
     public function __construct($firstName)
     {
