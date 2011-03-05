efc26f2aa59af4107adb56c8bbc55c6a33f6fe76
Jonathan H. Wage <jonwage@gmail.com>
2011-03-05 23:51:38 +0100

    Bumping version to 1.0.0RC1-DEV

--- lib/Doctrine/ODM/MongoDB/Version.php
+++ lib/Doctrine/ODM/MongoDB/Version.php
@@ -32,7 +32,7 @@ class Version
     /**
      * Current Doctrine Version
      */
-    const VERSION = '1.0.0BETA3-DEV';
+    const VERSION = '1.0.0RC1-DEV';
 
     /**
      * Compares a Doctrine version with the current one.
