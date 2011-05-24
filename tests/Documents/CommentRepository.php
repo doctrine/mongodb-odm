5c75c10b44fec7efca88d1bb700ac85778fba601
Kris Wallsmith <kris.wallsmith@gmail.com>
2011-05-24 09:43:32 -0700

    fixed strange reflection bug

--- tests/Documents/CommentRepository.php
+++ tests/Documents/CommentRepository.php
@@ -4,6 +4,7 @@ namespace Documents;
 
 use Doctrine\ODM\MongoDB\DocumentRepository;
 
+/** FIXME: reflection chokes if this class doesn't have a doc comment */
 class CommentRepository extends DocumentRepository
 {
     public function findOneComment()
