f1225c7d42f5fd03a23af7ac0f221a97ad6875ec
ornicar <thibault.duplessis@gmail.com>
2011-06-14 17:15:24 -0700

    Convert ID to PHP value to register the document

--- lib/Doctrine/ODM/MongoDB/Persisters/DocumentPersister.php
+++ lib/Doctrine/ODM/MongoDB/Persisters/DocumentPersister.php
@@ -464,7 +464,7 @@ class DocumentPersister
 
         if ($document !== null) {
             $hints[Query::HINT_REFRESH] = true;
-            $id = $result['_id'];
+            $id = $this->class->getPHPIdentifierValue($result['_id']);
             $this->uow->registerManaged($document, $id, $result);
         }
 
