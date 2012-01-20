f3ba4317bf61333c79ee2905651b82470fa36cff
Jonathan H. Wage <jonwage@gmail.com>
2012-01-20 01:44:35 -0600

    Add functionality to mark embedded field with reference back to parent.

--- lib/Doctrine/ODM/MongoDB/Persisters/DocumentPersister.php
+++ lib/Doctrine/ODM/MongoDB/Persisters/DocumentPersister.php
@@ -526,6 +526,7 @@ class DocumentPersister
     {
         $hints = $collection->getHints();
         $mapping = $collection->getMapping();
+        $owner = $collection->getOwner();
         $cmd = $this->cmd;
         $groupedIds = array();
 
@@ -539,6 +540,7 @@ class DocumentPersister
             }
             $id = (string) $mongoId;
             $reference = $this->dm->getReference($className, $id);
+            $this->uow->setParentAssociation($reference, $mapping, $owner, $mapping['name'].'.'.$key);
             if ($mapping['strategy'] === 'set') {
                 $collection->set($key, $reference);
             } else {
@@ -610,14 +612,18 @@ class DocumentPersister
             $qb->slaveOkay(true);
         }
         $documents = $qb->getQuery()->execute()->toArray();
+        $count = 0;
         foreach ($documents as $document) {
+            $count++;
             $collection->add($document);
+            $this->uow->setParentAssociation($document, $mapping, $owner, $mapping['name'].'.'.$count);
         }
     }
 
     private function loadReferenceManyWithRepositoryMethod(PersistentCollection $collection)
     {
         $mapping = $collection->getMapping();
+        $owner = $collection->getOwner();
         $cursor = $this->dm->getRepository($mapping['targetDocument'])->$mapping['repositoryMethod']($collection->getOwner());
         if ($mapping['sort']) {
             $cursor->sort($mapping['sort']);
@@ -632,8 +638,11 @@ class DocumentPersister
             $cursor->slaveOkay(true);
         }
         $documents = $cursor->toArray();
+        $count = 0;
         foreach ($documents as $document) {
+            $count++;
             $collection->add($document);
+            $this->uow->setParentAssociation($document, $mapping, $owner, $mapping['name'].'.'.$count);
         }
     }
 
