31273c9266a9cf8cad72e03eb5b16ae104ed1eff
Jonathan H. Wage <jonwage@gmail.com>
2012-01-22 16:28:26 -0600

    Add docs theme.

--- en/conf.py
+++ en/conf.py
@@ -93,6 +93,8 @@ pygments_style = 'sphinx'
 # Sphinx are currently 'default' and 'sphinxdoc'.
 html_theme = 'default'
 
+html_theme_dir = ['_theme']
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
