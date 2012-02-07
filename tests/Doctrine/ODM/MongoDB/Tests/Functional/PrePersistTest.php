7d05c39f35636efd245a46067a72a4ce1601f732
Jonathan H. Wage <jonwage@gmail.com>
2012-02-06 22:04:20 -0600

    Add prePersist() test.

---
+++ tests/Doctrine/ODM/MongoDB/Tests/Functional/PrePersistTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Doctrine\ODM\MongoDB\Tests\Functional;
+
+use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
+
+class PrePersistTest extends \Doctrine\ODM\MongoDB\Tests\BaseTest
+{
+    public function testPrePersist()
+    {
+        $test = new PrePersistTestDocument();
+        $this->dm->persist($test);
+        $this->dm->persist($test);
+        $this->dm->flush();
+        $this->dm->flush();
+
+        $this->assertEquals(1, $test->prePersist);
+
+        $test->field = 'test';
+
+        $this->dm->flush();
+        $this->dm->flush();
+
+        $this->assertEquals(1, $test->preUpdate);
+    }
+}
+
+/** @ODM\Document */
+class PrePersistTestDocument
+{
+    public $prePersist;
+    public $preUpdate;
+
+    /** @ODM\Id */
+    public $id;
+
+    /** @ODM\String */
+    public $field;
+
+    /** @ODM\PrePersist */
+    public function prePersist()
+    {
+        $this->prePersist++;
+    }
+
+    /** @ODM\PreUpdate */
+    public function preUpdate()
+    {
+        $this->preUpdate++;
+    }
+}
