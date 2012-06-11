439838239e5f2aa06c9e680ee93f046cb8cdcc3b
Thomas Adam <thomas.adam@tecbot.de>
2012-06-11 14:06:36 +0200

    fixed getIdentifierValue to return not only strings

--- lib/Doctrine/ODM/MongoDB/Mapping/ClassMetadataInfo.php
+++ lib/Doctrine/ODM/MongoDB/Mapping/ClassMetadataInfo.php
@@ -1328,7 +1328,7 @@ class ClassMetadataInfo implements \Doctrine\Common\Persistence\Mapping\ClassMet
         if ($document instanceof Proxy && !$document->__isInitialized()) {
             return $document->__identifier__;
         }
-        return (string) $this->reflFields[$this->identifier]->getValue($document);
+        return $this->reflFields[$this->identifier]->getValue($document);
     }
 
     /**
