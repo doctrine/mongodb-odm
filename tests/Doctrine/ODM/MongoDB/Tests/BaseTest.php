774e6b5631cca44c982f6c08c2fd97d92925c177
Jeremy Mikola <jmikola@gmail.com>
2012-06-20 16:50:22 +0100

    Drop collections in tearDown() instead of safe removal
    This cuts the test suite execution time in half.

--- tests/Doctrine/ODM/MongoDB/Tests/BaseTest.php
+++ tests/Doctrine/ODM/MongoDB/Tests/BaseTest.php
@@ -70,7 +70,7 @@ abstract class BaseTest extends \PHPUnit_Framework_TestCase
         if ($this->dm) {
             $collections = $this->dm->getConnection()->selectDatabase('doctrine_odm_tests')->listCollections();
             foreach ($collections as $collection) {
-                $collection->remove(array(), array('safe' => true));
+                $collection->drop();
             }
         }
     }
