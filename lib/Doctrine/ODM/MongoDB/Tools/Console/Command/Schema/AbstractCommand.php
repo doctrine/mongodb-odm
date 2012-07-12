677e5376f09011d82322e977a9fffb306dd2c7a2
Jeremy Mikola <jmikola@gmail.com>
2012-07-12 16:31:12 -0400

    Refactor create, drop and update console commands
    Moved code specific to create/drop into the respective classes, simplifying AbstractCommand. Also touched up messages.

--- lib/Doctrine/ODM/MongoDB/Tools/Console/Command/Schema/AbstractCommand.php
+++ lib/Doctrine/ODM/MongoDB/Tools/Console/Command/Schema/AbstractCommand.php
@@ -2,82 +2,17 @@
 
 namespace Doctrine\ODM\MongoDB\Tools\Console\Command\Schema;
 
-use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input;
-use Symfony\Component\Console\Output;
 use Doctrine\ODM\MongoDB\SchemaManager;
+use Symfony\Component\Console\Command\Command;
 
 /**
  * @author Bulat Shakirzyanov <mallluhuct@gmail.com>
  */
 abstract class AbstractCommand extends Command
 {
-    const DBS = 'db';
-    const COLLECTIONS = 'collection';
-    const INDEXES = 'index';
-
-    /**
-     * Dropping and Replacement of schema happens in reverse order (indexes <- collections <- dbs)
-     *
-     * @var array
-     */
-    protected $availableOptions = array(self::INDEXES, self::COLLECTIONS, self::DBS);
-
-    protected $commandName;
-
-    protected function configure()
-    {
-        $this
-            ->setName('odm:schema:' . $this->commandName)
-            ->setDescription("Allows you to $this->commandName databases, collections and indexes for your documents")
-            ->setDefinition(array(
-                new Input\InputOption('class', 'c', Input\InputOption::VALUE_OPTIONAL, 'The class name to create database, collection and indexes for, all classes will be used if none specified', null),
-            ))
-        ;
-
-        foreach ($this->availableOptions as $option) {
-            $this->addOption($option, null, Input\InputOption::VALUE_NONE, ucfirst($this->commandName) . ' ' . $option);
-        }
-    }
-
-    protected function execute(Input\InputInterface $input, Output\OutputInterface $output)
-    {
-        $none = true;
-        foreach ($this->availableOptions as $option) {
-            if (false !== $input->getOption($option)) {
-                $none = false;
-            }
-        }
-
-        $class = $input->getOption('class');
-        $sm = $this->getSchemaManager();
-        foreach ($this->availableOptions as $option) {
-            if (false !== $input->getOption($option) || $none === true) {
-                try {
-                    if (isset($class)) {
-                        $this->{'processDocument' . ucfirst($option)}($sm, $class);
-                    } else {
-                        $this->{'process' . ucfirst($option)}($sm);
-                    }
-                    switch ($this->commandName) {
-                        case 'update':
-                            $commandVerb = 'Updated';
-                            break;
-                        case 'drop':
-                            $commandVerb = 'Dropped';
-                            break;
-                        case 'create':
-                        default:
-                            $commandVerb = 'Created';
-                            break;
-                    }
-                    $output->writeln(sprintf('%s <comment>%s</comment> for <info>%s</info>', $commandVerb, $option, (isset($class) ? $class : 'all classes')));
-                } catch (\Exception $e) {
-                    $output->writeln('<error>' . $e->getMessage() . '</error>');
-                }
-            }
-        }
-    }
+    const DB = 'db';
+    const COLLECTION = 'collection';
+    const INDEX = 'index';
 
     abstract protected function processDocumentCollection(SchemaManager $sm, $document);
     abstract protected function processCollection(SchemaManager $sm);
--- lib/Doctrine/ODM/MongoDB/Tools/Console/Command/Schema/UpdateCommand.php
+++ lib/Doctrine/ODM/MongoDB/Tools/Console/Command/Schema/UpdateCommand.php
@@ -3,19 +3,43 @@
 namespace Doctrine\ODM\MongoDB\Tools\Console\Command\Schema;
 
 use Doctrine\ODM\MongoDB\SchemaManager;
-use Symfony\Component\Console\Input;
-use Symfony\Component\Console\Output;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
 
 /**
  * @author Chris Jones <leeked@gmail.com>
  */
 class UpdateCommand extends AbstractCommand
 {
-    /**
-     * @var array
-     */
-    protected $availableOptions = array(parent::INDEXES);
-    protected $commandName = 'update';
+    private $dropOrder = array(self::INDEX, self::COLLECTION, self::DB);
+
+    protected function configure()
+    {
+        $this
+            ->setName('odm:schema:update')
+            ->addOption('class', 'c', InputOption::VALUE_OPTIONAL, 'Document class to process (default: all classes)')
+            ->setDescription('Update indexes for your documents')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $class = $input->getOption('class');
+        $sm = $this->getSchemaManager();
+
+        try {
+            if (isset($class)) {
+                $this->processDocumentIndex($sm, $class);
+                $output->writeln(sprintf('Updated <comment>index(es)</comment> for <info>%s</info>', $class));
+            } else {
+                $this->processIndex($sm);
+                $output->writeln('Updated <comment>indexes</comment> for <info>all classes</info>');
+            }
+        } catch (\Exception $e) {
+            $output->writeln('<error>' . $e->getMessage() . '</error>');
+        }
+    }
 
     protected function processDocumentIndex(SchemaManager $sm, $document)
     {
@@ -29,21 +53,21 @@ class UpdateCommand extends AbstractCommand
 
     protected function processDocumentCollection(SchemaManager $sm, $document)
     {
-        throw new \RuntimeException('Cannot update a document collection');
+        throw new \BadMethodCallException('Cannot update a document collection');
     }
 
     protected function processCollection(SchemaManager $sm)
     {
-        throw new \RuntimeException('Cannot update a collection');
+        throw new \BadMethodCallException('Cannot update a collection');
     }
 
     protected function processDocumentDb(SchemaManager $sm, $document)
     {
-        throw new \RuntimeException('Cannot update a document database');
+        throw new \BadMethodCallException('Cannot update a document database');
     }
 
     protected function processDb(SchemaManager $sm)
     {
-        throw new \RuntimeException('Cannot update a database');
+        throw new \BadMethodCallException('Cannot update a database');
     }
 }
