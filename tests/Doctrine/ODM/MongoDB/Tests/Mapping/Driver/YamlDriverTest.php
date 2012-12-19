c17ee1c30c869763e327de905f10f92b8b37e2c3
Jeremy Mikola <jmikola@gmail.com>
2012-12-18 21:11:43 -0500

    Make Yaml component an optional dependency

--- tests/Doctrine/ODM/MongoDB/Tests/Mapping/Driver/YamlDriverTest.php
+++ tests/Doctrine/ODM/MongoDB/Tests/Mapping/Driver/YamlDriverTest.php
@@ -11,6 +11,10 @@ class YamlDriverTest extends AbstractDriverTest
 {
     public function setUp()
     {
+        if (!class_exists('Symfony\Component\Yaml\Yaml', true)) {
+            $this->markTestSkipped('This test requires the Symfony YAML component');
+        }
+
         $this->driver = new YamlDriver(__DIR__ . '/fixtures/yaml');
     }
 }
--- tests/Doctrine/ODM/MongoDB/Tests/Mapping/YamlMappingDriverTest.php
+++ tests/Doctrine/ODM/MongoDB/Tests/Mapping/YamlMappingDriverTest.php
@@ -9,7 +9,7 @@ class YamlMappingDriverTest extends AbstractMappingDriverTest
     protected function _loadDriver()
     {
         if (!class_exists('Symfony\Component\Yaml\Yaml', true)) {
-            $this->markTestSkipped('Please install Symfony YAML Component into the include path of your PHP installation.');
+            $this->markTestSkipped('This test requires the Symfony YAML component');
         }
 
         return new YamlDriver(__DIR__ . DIRECTORY_SEPARATOR . 'yaml');
