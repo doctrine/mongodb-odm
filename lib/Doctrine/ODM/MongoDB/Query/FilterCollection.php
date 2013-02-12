2ac9c0cf0f9f4c03af169bfdb53e7c38d5a16a22
Wesley van Opdorp <wesley.van.opdorp@freshheads.com>
2013-02-12 14:09:42 +0100

    Removed hasFilter boolean check

--- lib/Doctrine/ODM/MongoDB/Query/FilterCollection.php
+++ lib/Doctrine/ODM/MongoDB/Query/FilterCollection.php
@@ -87,7 +87,7 @@ class FilterCollection
             throw new \InvalidArgumentException("Filter '" . $name . "' does not exist.");
         }
 
-        if (true !== $this->hasFilter($name)) {
+        if ($this->hasFilter($name)) {
             $this->enabledFilters[$name] = new $filterClass($this->dm);
         }
 
@@ -122,7 +122,7 @@ class FilterCollection
      */
     public function getFilter($name)
     {
-        if (true !== $this->hasFilter($name)) {
+        if ($this->hasFilter($name)) {
             throw new \InvalidArgumentException("Filter '" . $name . "' is not enabled.");
         }
         return $this->enabledFilters[$name];
