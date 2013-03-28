c2158f85ccbbbedac410f6ffc4686c72bdc3477a
Thomas Adam <thomas.adam@tecbot.de>
2013-03-28 08:03:07 +0100

    added "documentNotFound" event to hook in the proxy initialization process if its failed

---
+++ lib/Doctrine/ODM/MongoDB/Event/DocumentNotFoundEventArgs.php
@@ -0,0 +1,99 @@
+<?php
+/*
+ *  $Id$
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license. For more information, see
+ * <http://www.doctrine-project.org>.
+ */
+
+namespace Doctrine\ODM\MongoDB\Event;
+
+use Doctrine\ODM\MongoDB\DocumentManager;
+use Doctrine\Common\EventArgs;
+
+/**
+ * Provides event arguments for the documentNotFound event.
+ *
+ * @since       1.0
+ * @author      Thomas Adam <thomas.adam@tecbot.de>
+ */
+class DocumentNotFoundEventArgs extends EventArgs
+{
+    /**
+     * @var object
+     */
+    private $proxy;
+    /**
+     * @var string
+     */
+    private $identifier;
+    /**
+     * @var Boolean
+     */
+    private $initialized = false;
+
+    /**
+     * Constructor.
+     *
+     * @param object $proxy
+     * @param string $identifier
+     */
+    public function __construct($proxy, $identifier)
+    {
+        $this->proxy = $proxy;
+        $this->identifier = $identifier;
+    }
+
+    /**
+     * Retrieve associated proxy.
+     *
+     * @return object
+     */
+    public function getProxy()
+    {
+        return $this->proxy;
+    }
+
+    /**
+     * Retrieve associated identifier.
+     *
+     * @return string
+     */
+    public function getIdentifier()
+    {
+        return $this->identifier;
+    }
+
+    /**
+     * Returns whether the proxy is initialized.
+     *
+     * @return Boolean
+     */
+    public function isInitialized()
+    {
+        return $this->initialized;
+    }
+
+    /**
+     * Sets whether the proxy is initialized.
+     *
+     * @param Boolean
+     */
+    public function setInitialized($initialized)
+    {
+        $this->initialized = $initialized;
+    }
+}
