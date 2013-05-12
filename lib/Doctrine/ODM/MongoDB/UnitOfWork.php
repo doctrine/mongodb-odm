03e2cf122c7782176809da2c63335a9a2ea06d09
spujadas <spujadas+github@gmail.com>
2013-05-12 17:41:22 +0200

    fixed incomplete UnitOfWork::clear()

--- lib/Doctrine/ODM/MongoDB/UnitOfWork.php
+++ lib/Doctrine/ODM/MongoDB/UnitOfWork.php
@@ -2430,6 +2430,7 @@ class UnitOfWork implements PropertyChangedListener
             $this->documentDeletions =
             $this->collectionUpdates =
             $this->collectionDeletions =
+            $this->persisters =
             $this->extraUpdates =
             $this->parentAssociations =
             $this->orphanRemovals = array();
