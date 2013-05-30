7a5f0c4635eeb94dda940e310fbf1cec4fb8cba3
Jordan Stout <j@jrdn.org>
2013-05-30 11:56:24 -0700

    remove filter criteria from query as it is handled in the DocumentPersister

--- lib/Doctrine/ODM/MongoDB/Query/Query.php
+++ lib/Doctrine/ODM/MongoDB/Query/Query.php
@@ -207,10 +207,6 @@ class Query extends \Doctrine\MongoDB\Query\Query
             throw MongoDBException::queryNotIndexed($this->class->name, $this->getUnindexedFields());
         }
 
-        $this->query['query'] = array_merge(
-            $this->query['query'],
-            $this->dm->getFilterCollection()->getFilterCriteria($this->class)
-        );
         $results = parent::execute();
 
         $hints = array();
