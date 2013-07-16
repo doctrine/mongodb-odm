358618f04fadf4670db0b4eefe2b5ae816886806
Frederic G. MARAND <fgm@osinet.fr>
2013-07-16 11:34:28 +0200

    Fixes CLI using $helpers as a hash with cli-config defining it as an array.
    - Define it as the expected array('dm' => helper) hash.
    - This caused "[InvalidArgumentException] The helper "dm" is not defined."

--- tools/sandbox/cli-config.php
+++ tools/sandbox/cli-config.php
@@ -4,4 +4,4 @@ use Doctrine\ODM\MongoDB\Tools\Console\Helper\DocumentManagerHelper;
 
 require_once 'config.php';
 
-$helpers = array(new DocumentManagerHelper($dm));
+$helpers = array('dm' => new DocumentManagerHelper($dm));
