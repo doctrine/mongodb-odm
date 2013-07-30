42909efe19a4b78e2d0b7076f72f3a8b90ad0b19
Jeremy Mikola <jmikola@gmail.com>
2013-07-30 14:09:54 -0400

    Refactor regression test for #561

--- tests/Doctrine/ODM/MongoDB/Tests/Functional/Ticket/GH561Test.php
+++ tests/Doctrine/ODM/MongoDB/Tests/Functional/Ticket/GH561Test.php
@@ -5,119 +5,69 @@ namespace Doctrine\ODM\MongoDB\Tests\Functional\Ticket;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
 
-/**
- *
- * @group GH561
- */
 class GH561Test extends \Doctrine\ODM\MongoDB\Tests\BaseTest
 {
     public function testPersistMainDocument()
     {
-        $embeddedDocument = new GH561TestEmbeddedDocument('test');
+        $embeddedDocument = new GH561EmbeddedDocument();
+        $embeddedDocument->embeddedDocuments->add(new GH561AnotherEmbeddedDocument('foo'));
 
-        $anotherEmbeddedDocuments = array(
-            new GH561AnotherEmbedded('doc1'),
-            new GH561AnotherEmbedded('doc2'),
-            new GH561AnotherEmbedded('doc3')
-        );
-        $embeddedDocument->setEmbeddedDocuments($anotherEmbeddedDocuments);
+        $document = new GH561Document();
+        $document->embeddedDocuments->add($embeddedDocument);
 
-        $embeddedDocuments = array($embeddedDocument);
-        $testDoc = new GH561TestDocument();
-        $testDoc->setEmbeddedDocuments($embeddedDocuments);
-
-        $this->dm->persist($testDoc);
+        $this->dm->persist($document);
         $this->dm->flush();
+        $this->dm->clear();
 
-        $testDoc = $this->dm->find(__NAMESPACE__.'\GH561TestDocument', $testDoc->id);
-        $this->assertEquals($embeddedDocuments, $testDoc->embeddedDocuments->toArray());
+        $document = $this->dm->find(__NAMESPACE__.'\GH561Document', $document->id);
+        $this->assertInstanceOf(__NAMESPACE__.'\GH561Document', $document);
+        $this->assertCount(1, $document->embeddedDocuments);
 
-        $embeddedDocument = $testDoc->embeddedDocuments->first();
-        $this->assertTrue($embeddedDocument instanceof GH561TestEmbeddedDocument);
-        $this->assertEquals($anotherEmbeddedDocuments, $embeddedDocument->otherEmbedded->toArray());
+        $embeddedDocument = $document->embeddedDocuments->first();
+        $this->assertInstanceOf(__NAMESPACE__.'\GH561EmbeddedDocument', $embeddedDocument);
+        $this->assertCount(1, $embeddedDocument->embeddedDocuments);
 
-        $anotherEmbeddedDocument = $embeddedDocument->otherEmbedded->first();
-        $this->assertTrue($anotherEmbeddedDocument instanceof GH561AnotherEmbedded);
-        $this->assertEquals('doc1', $anotherEmbeddedDocument->something);
+        $anotherEmbeddedDocument = $embeddedDocument->embeddedDocuments->first();
+        $this->assertInstanceOf(__NAMESPACE__.'\GH561AnotherEmbeddedDocument', $anotherEmbeddedDocument);
+        $this->assertEquals('foo', $anotherEmbeddedDocument->name);
     }
 }
 
 /** @ODM\Document */
-class GH561TestDocument
+class GH561Document
 {
     /** @ODM\Id */
     public $id;
 
-    // Note: Test case fails with default "pushAll" strategy, but "set" works
-    /** @ODM\EmbedMany(targetDocument="GH561TestEmbeddedDocument", strategy="set") */
+    /** @ODM\EmbedMany(targetDocument="GH561EmbeddedDocument", strategy="set") */
     public $embeddedDocuments;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->embeddedDocuments = new ArrayCollection();
-    }
-
-    /**
-     * Sets children
-     *
-     * If $images is not an array or Traversable object, this method will simply
-     * clear the images collection property.  If any elements in the parameter
-     * are not an Image object, this method will attempt to convert them to one
-     * by mapping array indexes (size URL's are required, cropMetadata is not).
-     * Any invalid elements will be ignored.
-     *
-     * @param array|Traversable $children
-     */
-    public function setEmbeddedDocuments($embeddedDocuments) {
-        $this->embeddedDocuments->clear();
-
-        if (! (is_array($embeddedDocuments) || $embeddedDocuments instanceof \Traversable)) {
-            return;
-        }
-
-        foreach ($embeddedDocuments as $embeddedDocument) {
-            $this->embeddedDocuments->add($embeddedDocument);
-        }
     }
 }
 
 /** @ODM\EmbeddedDocument */
-class GH561TestEmbeddedDocument
+class GH561EmbeddedDocument
 {
+    /** @ODM\EmbedMany(targetDocument="GH561AnotherEmbeddedDocument", strategy="set") */
+    public $embeddedDocuments;
+
     public function __construct()
     {
-        $this->otherEmbedded = new \Doctrine\Common\Collections\ArrayCollection();
-    }
-
-    /**
-     * @@ODM\EmbedMany(targetDocument="AnotherEmbedded", strategy="set")
-     */
-    public $otherEmbedded;
-
-    public function setEmbeddedDocuments($embeddedDocuments) {
-        $this->otherEmbedded->clear();
-
-        if (! (is_array($embeddedDocuments) || $embeddedDocuments instanceof \Traversable)) {
-            return;
-        }
-
-        foreach ($embeddedDocuments as $embeddedDocument) {
-            $this->otherEmbedded->add($embeddedDocument);
-        }
+        $this->embeddedDocuments = new ArrayCollection();
     }
 }
 
-/**
- * @@ODM\EmbeddedDocument
- */
-class GH561AnotherEmbedded
+/** @ODM\EmbeddedDocument */
+class GH561AnotherEmbeddedDocument
 {
-    public function __construct($string)
-    {
-        $this->something = $string;
-    }
+    /** @ODM\String */
+    public $name;
 
-    /**
-     * @@ODM\String
-     */
-    public $something;
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
 }
