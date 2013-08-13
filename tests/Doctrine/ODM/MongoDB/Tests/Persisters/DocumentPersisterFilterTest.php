301f794fea252d97aa7f76b2af348b848c2302d4
Tim Roediger <superdweebie@gmail.com>
2013-08-13 12:44:19 +1000

    Add query criteria merge function

--- tests/Doctrine/ODM/MongoDB/Tests/Persisters/DocumentPersisterFilterTest.php
+++ tests/Doctrine/ODM/MongoDB/Tests/Persisters/DocumentPersisterFilterTest.php
@@ -32,6 +32,36 @@ class DocumentPersisterFilterTest extends BaseTest
 
         $criteria = $persister->addFilterToPreparedQuery(array('username' => 'Toby'));
 
-        $this->assertEquals('Tim', $criteria['username']);
+        $this->assertEquals(['Toby', 'Tim'], $criteria['username']);
+    }
+
+    public function testFilterCrieriaShouldOverrideMappingCriteriaOwningSide(){
+
+        //create some data to test against
+        $blogPost = new \Documents\BlogPost('Roger');
+        $blogPost->addComment(new \Documents\Comment('comment by normal user', new \DateTime(), false));
+        $blogPost->addComment(new \Documents\Comment('comment by admin', new \DateTime(), true));
+
+        $dm = $this->dm;
+        $dm->persist($blogPost);
+        $dm->flush();
+        $id = $blogPost->id;
+        $dm->clear();
+
+        //enable the filter
+        $this->fc->enable('testFilter');
+        $testFilter = $this->fc->getFilter('testFilter');
+        $testFilter->setParameter('class', 'Documents\Comment');
+        $testFilter->setParameter('field', 'isByAdmin');
+        $testFilter->setParameter('value', false);
+
+        //test the filter
+        $blogPost = $dm->getRepository('Documents\BlogPost')->find($id);
+
+        $this->assertCount(1, $blogPost->comments); //the admin comment should be removed by the filter
+        $this->assertCount(0, $blogPost->adminComments); //the user comment should be removed by the mapping criteria, and the admin comment by the filter
+
+        $dm->remove($blogPost);
+        $dm->flush();
     }
 }
