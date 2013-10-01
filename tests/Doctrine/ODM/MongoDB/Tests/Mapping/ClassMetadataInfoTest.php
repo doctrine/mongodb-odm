5b68ab6eae60af5953cc884266cab5c8c1dcabd6
Tim Roediger <superdweebie@gmail.com>
2013-10-01 14:18:42 +1000

    ClassMetadata support for getting/setting fields on proxy objects

--- tests/Doctrine/ODM/MongoDB/Tests/Mapping/ClassMetadataInfoTest.php
+++ tests/Doctrine/ODM/MongoDB/Tests/Mapping/ClassMetadataInfoTest.php
@@ -4,7 +4,7 @@ namespace Doctrine\ODM\MongoDB\Tests\Mapping;
 
 use Doctrine\ODM\MongoDB\Mapping\ClassMetadataInfo;
 
-class ClassMetadataInfoTest extends \PHPUnit_Framework_TestCase
+class ClassMetadataInfoTest extends \Doctrine\ODM\MongoDB\Tests\BaseTest
 {
     public function testDefaultDiscriminatorField()
     {
@@ -50,5 +50,93 @@ class ClassMetadataInfoTest extends \PHPUnit_Framework_TestCase
             'type', $mapping['discriminatorField'],
             'Default discriminator field is not set for associations with discriminatorField option'
         );
+    }
+
+    public function testGetFieldValue()
+    {
+        //load some metadata
+        $metadata = $this->dm->getClassMetadata('Documents\Album');
+
+        //create a document
+        $document = new \Documents\Album('ten');
+
+        $this->assertEquals('ten', $metadata->getFieldValue($document, 'name'));
+    }
+
+    public function testGetFieldValueWithProxy()
+    {
+        //create and persist a document
+        $document = new \Documents\Album('ten');
+        $this->dm->persist($document);
+        $this->dm->flush();
+        $this->dm->clear();
+
+        //get the proxy
+        $proxy = $this->dm->getReference('Documents\Album', $document->getId());
+
+        //get metadata
+        $metadata = $this->dm->getClassMetadata('Documents\Album');
+
+        $this->assertEquals('ten', $metadata->getFieldValue($proxy, 'name'));
+    }
+
+    public function testGetIdentifierFieldValueWithProxy()
+    {
+        //create and persist a document
+        $document = new \Documents\Album('ten');
+        $this->dm->persist($document);
+        $this->dm->flush();
+        $this->dm->clear();
+
+        //get the proxy
+        $proxy = $this->dm->getReference('Documents\Album', $document->getId());
+
+        //get metadata
+        $metadata = $this->dm->getClassMetadata('Documents\Album');
+
+        $metadata->getFieldValue($proxy, 'id');
+        
+        $this->assertFalse($proxy->__isInitialized());
+    }
+
+    public function testSetFieldValue()
+    {
+        //load some metadata
+        $metadata = $this->dm->getClassMetadata('Documents\Album');
+
+        //create a document
+        $document = new \Documents\Album('ten');
+
+        //set a field
+        $metadata->setFieldValue($document, 'name', 'nevermind');
+
+        $this->assertEquals('nevermind', $document->getName());
+    }
+
+    public function testSetFieldValueWithProxy()
+    {
+        //create and persist a document
+        $document = new \Documents\Album('ten');
+        $this->dm->persist($document);
+        $this->dm->flush();
+        $this->dm->clear();
+
+        //get the proxy
+        $proxy = $this->dm->getReference('Documents\Album', $document->getId());
+
+        //get metadata
+        $metadata = $this->dm->getClassMetadata('Documents\Album');
+
+        //set a field
+        $metadata->setFieldValue($proxy, 'name', 'nevermind');
+
+        //flush changes
+        $this->dm->flush();
+        $this->dm->clear();
+
+        //check that changes did get flushed
+        $proxy = $this->dm->getReference('Documents\Album', $document->getId());
+
+        $this->assertEquals('nevermind', $proxy->getName());
     }
 }
