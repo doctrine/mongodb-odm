3fde10a4c07eb36f9932a51949ff95bc1f94b603
Jeremy Mikola <jmikola@gmail.com>
2013-12-31 19:36:44 -0500

    Refactor proxy setter/getter tests ClassMetadataInfoTest

--- tests/Doctrine/ODM/MongoDB/Tests/Mapping/ClassMetadataInfoTest.php
+++ tests/Doctrine/ODM/MongoDB/Tests/Mapping/ClassMetadataInfoTest.php
@@ -3,6 +3,7 @@
 namespace Doctrine\ODM\MongoDB\Tests\Mapping;
 
 use Doctrine\ODM\MongoDB\Mapping\ClassMetadataInfo;
+use Documents\Album;
 
 class ClassMetadataInfoTest extends \Doctrine\ODM\MongoDB\Tests\BaseTest
 {
@@ -54,60 +55,47 @@ class ClassMetadataInfoTest extends \Doctrine\ODM\MongoDB\Tests\BaseTest
 
     public function testGetFieldValue()
     {
-        //load some metadata
+        $document = new Album('ten');
         $metadata = $this->dm->getClassMetadata('Documents\Album');
 
-        //create a document
-        $document = new \Documents\Album('ten');
-
-        $this->assertEquals('ten', $metadata->getFieldValue($document, 'name'));
+        $this->assertEquals($document->getName(), $metadata->getFieldValue($document, 'name'));
     }
 
-    public function testGetFieldValueWithProxy()
+    public function testGetFieldValueInitializesProxy()
     {
-        //create and persist a document
-        $document = new \Documents\Album('ten');
+        $document = new Album('ten');
         $this->dm->persist($document);
         $this->dm->flush();
         $this->dm->clear();
 
-        //get the proxy
         $proxy = $this->dm->getReference('Documents\Album', $document->getId());
-
-        //get metadata
         $metadata = $this->dm->getClassMetadata('Documents\Album');
 
-        $this->assertEquals('ten', $metadata->getFieldValue($proxy, 'name'));
+        $this->assertEquals($document->getName(), $metadata->getFieldValue($proxy, 'name'));
+        $this->assertInstanceOf('Doctrine\ODM\MongoDB\Proxy\Proxy', $proxy);
+        $this->assertTrue($proxy->__isInitialized());
     }
 
-    public function testGetIdentifierFieldValueWithProxy()
+    public function testGetFieldValueOfIdentifierDoesNotInitializeProxy()
     {
-        //create and persist a document
-        $document = new \Documents\Album('ten');
+        $document = new Album('ten');
         $this->dm->persist($document);
         $this->dm->flush();
         $this->dm->clear();
 
-        //get the proxy
         $proxy = $this->dm->getReference('Documents\Album', $document->getId());
-
-        //get metadata
         $metadata = $this->dm->getClassMetadata('Documents\Album');
 
-        $metadata->getFieldValue($proxy, 'id');
-        
+        $this->assertEquals($document->getId(), $metadata->getFieldValue($proxy, 'id'));
+        $this->assertInstanceOf('Doctrine\ODM\MongoDB\Proxy\Proxy', $proxy);
         $this->assertFalse($proxy->__isInitialized());
     }
 
     public function testSetFieldValue()
     {
-        //load some metadata
+        $document = new Album('ten');
         $metadata = $this->dm->getClassMetadata('Documents\Album');
 
-        //create a document
-        $document = new \Documents\Album('ten');
-
-        //set a field
         $metadata->setFieldValue($document, 'name', 'nevermind');
 
         $this->assertEquals('nevermind', $document->getName());
@@ -115,27 +103,22 @@ class ClassMetadataInfoTest extends \Doctrine\ODM\MongoDB\Tests\BaseTest
 
     public function testSetFieldValueWithProxy()
     {
-        //create and persist a document
-        $document = new \Documents\Album('ten');
+        $document = new Album('ten');
         $this->dm->persist($document);
         $this->dm->flush();
         $this->dm->clear();
 
-        //get the proxy
         $proxy = $this->dm->getReference('Documents\Album', $document->getId());
+        $this->assertInstanceOf('Doctrine\ODM\MongoDB\Proxy\Proxy', $proxy);
 
-        //get metadata
         $metadata = $this->dm->getClassMetadata('Documents\Album');
-
-        //set a field
         $metadata->setFieldValue($proxy, 'name', 'nevermind');
 
-        //flush changes
         $this->dm->flush();
         $this->dm->clear();
 
-        //check that changes did get flushed
         $proxy = $this->dm->getReference('Documents\Album', $document->getId());
+        $this->assertInstanceOf('Doctrine\ODM\MongoDB\Proxy\Proxy', $proxy);
 
         $this->assertEquals('nevermind', $proxy->getName());
     }
