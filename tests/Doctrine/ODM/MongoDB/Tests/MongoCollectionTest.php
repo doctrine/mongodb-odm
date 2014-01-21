0518822119d5054e560f1c926e5af990460b6864
Hennadiy Verkh <h.verkh@traveltrex.com>
2014-01-21 11:01:30 +0100

    Added unit test for testing correct readPreference inheritance #759.

--- tests/Doctrine/ODM/MongoDB/Tests/MongoCollectionTest.php
+++ tests/Doctrine/ODM/MongoDB/Tests/MongoCollectionTest.php
@@ -2,6 +2,8 @@
 
 namespace Doctrine\ODM\MongoDB\Tests;
 
+use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
+
 /**
  * @author Bulat Shakirzyanov <mallluhuct@gmail.com>
  */
@@ -12,4 +14,32 @@ class MongoCollectionTest extends \Doctrine\ODM\MongoDB\Tests\BaseTest
         $mongoCollection = $this->dm->getDocumentCollection('Documents\File');
         $this->assertNull($mongoCollection->findOne(array('_id' => 'definitelynotanid')));
     }
+
+    public function testGetDocumentCollectionRespectsReadPreferenceInheritance(){
+        $expectedReadPreference = array(
+            'type' => \MongoClient::RP_SECONDARY_PREFERRED
+        );
+
+        $this->dm->getConnection()->setReadPreference(\MongoClient::RP_SECONDARY_PREFERRED);
+        $mongoCollection = $this->dm->getDocumentCollection(__NAMESPACE__ . '\ReadPreferenceTestDocument');
+
+        $connectionReadPreference = $this->dm->getConnection()->getReadPreference();
+        $dbReadPreference = $mongoCollection->getDatabase()->getReadPreference();
+        $collectionReadPreference = $mongoCollection->getReadPreference();
+
+        // All read preferences have to be the same
+        $this->assertEquals($expectedReadPreference, $connectionReadPreference);
+        $this->assertEquals($expectedReadPreference, $dbReadPreference);
+        $this->assertEquals($expectedReadPreference, $collectionReadPreference);
+    }
+}
+
+/** @ODM\Document */
+class ReadPreferenceTestDocument
+{
+    /** @ODM\Id */
+    public $id;
+
+    /** @ODM\String */
+    public $test;
 }
