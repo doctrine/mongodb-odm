doctrine/mongodb-odm

commit a49d20d1906523bc4845c89666bef9f41794f1b1

Clean up sandbox scripts
The only functional changes are relying on Composer to autoload example document classes and simplying the HelperSet configuration.

tools/sandbox/mongodb.php
@@ -1,19 +1,13 @@
 <?php
 
+use Doctrine\ODM\MongoDB\Version;
 use Symfony\Component\Console\Application;
-use Symfony\Component\Console\Helper\HelperSet;
 
-require __DIR__ . DIRECTORY_SEPARATOR . 'cli-config.php';
+require __DIR__ . '/cli-config.php';
 
-$helperSet = isset($helperSet) ? $helperSet : new HelperSet();
-foreach ($helpers as $name => $helper) {
-    $helperSet->set($helper, $name);
-}
+$app = new Application('Doctrine MongoDB ODM', Version::VERSION);
 
-$cli = new Application('Doctrine ODM MongoDB Command Line Interface', Doctrine\ODM\MongoDB\Version::VERSION);
-$cli->setCatchExceptions(true);
-$cli->setHelperSet($helperSet);
-$cli->addCommands(array(
+$app->addCommands(array(
     new \Doctrine\ODM\MongoDB\Tools\Console\Command\QueryCommand(),
     new \Doctrine\ODM\MongoDB\Tools\Console\Command\GenerateDocumentsCommand(),
     new \Doctrine\ODM\MongoDB\Tools\Console\Command\GenerateRepositoriesCommand(),
@@ -23,4 +17,9 @@ $cli->addCommands(array(
     new \Doctrine\ODM\MongoDB\Tools\Console\Command\Schema\DropCommand(),
     new \Doctrine\ODM\MongoDB\Tools\Console\Command\Schema\UpdateCommand(),
 ));
-$cli->run();
+
+if (isset($helperSet)) {
+    $app->setHelperSet($helperSet);
+}
+
+$app->run();

tools/sandbox/cli-config.php
@@ -1,7 +1,10 @@
 <?php
 
 use Doctrine\ODM\MongoDB\Tools\Console\Helper\DocumentManagerHelper;
+use Symfony\Component\Console\Helper\HelperSet;
 
-require_once 'config.php';
+require_once __DIR__ . '/config.php';
 
-$helpers = array('dm' => new DocumentManagerHelper($dm));
+$helperSet = new HelperSet(array(
+    'dm' => new DocumentManagerHelper($dm),
+));

tools/sandbox/config.php
@@ -1,36 +1,29 @@
 <?php
 
-use Doctrine\Common\ClassLoader;
 use Doctrine\MongoDB\Connection;
 use Doctrine\ODM\MongoDB\Configuration;
 use Doctrine\ODM\MongoDB\DocumentManager;
 use Doctrine\ODM\MongoDB\Mapping\Driver\AnnotationDriver;
 
-$file = __DIR__.'/../../vendor/autoload.php';
-if (!file_exists($file)) {
-    throw new RuntimeException('Install dependencies to run the sandbox.');
+if (!file_exists($file = __DIR__.'/../../vendor/autoload.php')) {
+    throw new RuntimeException('Install dependencies to run this script.');
 }
 
-require_once $file;
+$loader = require_once $file;
+$loader->add('Documents', __DIR__);
+
+$connection = new Connection();
+
+$config = new Configuration();
+$config->setProxyDir(__DIR__ . '/Proxies');
+$config->setProxyNamespace('Proxies');
+$config->setHydratorDir(__DIR__ . '/Hydrators');
+$config->setHydratorNamespace('Hydrators');
+$config->setDefaultDB('doctrine_odm_sandbox');
+// $config->setLoggerCallable(function(array $log) { print_r($log); });
+// $config->setMetadataCacheImpl(new Doctrine\Common\Cache\ApcCache());
+$config->setMetadataDriverImpl(AnnotationDriver::create(__DIR__ . '/Documents'));
 
 AnnotationDriver::registerAnnotationClasses();
 
-$classLoader = new ClassLoader('Documents', __DIR__);
-$classLoader->register();
-
-$config = new Configuration();
-
-$config->setProxyDir(__DIR__ . '/Proxies');
-$config->setProxyNamespace('Proxies');
-
-$config->setHydratorDir(__DIR__ . '/Hydrators');
-$config->setHydratorNamespace('Hydrators');
-
-$config->setDefaultDB('doctrine_odm_sandbox');
-
-// $config->setLoggerCallable(function(array $log) { print_r($log); });
-// $config->setMetadataCacheImpl(new Doctrine\Common\Cache\ApcCache());
-
-$config->setMetadataDriverImpl(AnnotationDriver::create(__DIR__ . '/Documents'));
-
-$dm = DocumentManager::create(new Connection(), $config);
+$dm = DocumentManager::create($connection, $config);

tools/sandbox/index.php
@@ -1,10 +1,10 @@
 <?php
 
-require_once 'config.php';
+require_once __DIR__ . '/config.php';
 
-use Documents\User;
+use Documents\Account;
 use Documents\Address;
 use Documents\Phonenumber;
-use Documents\Account;
+use Documents\User;
 
 // Your code here...

tools/sandbox/mongodb
@@ -1,4 +1,4 @@
 #!/usr/bin/env php
 <?php
 
-include('mongodb.php');
+require_once __DIR__ . '/mongodb.php';

commit 4b2a1d444271081338acf1e25825e48a4e5c3670

Application::setHelperSet() must be called before addCommands()
Application::add() calls Command::setApplication(), which in turn assigns the Application's current HelperSet to the Command. Therefore, we need to set the HelperSet before registering any Commands.

This fixes a bug that was introduced in a49d20d1906523bc4845c89666bef9f41794f1b1.

tools/sandbox/mongodb.php
@@ -7,6 +7,10 @@ require __DIR__ . '/cli-config.php';
 
 $app = new Application('Doctrine MongoDB ODM', Version::VERSION);
 
+if (isset($helperSet)) {
+    $app->setHelperSet($helperSet);
+}
+
 $app->addCommands(array(
     new \Doctrine\ODM\MongoDB\Tools\Console\Command\QueryCommand(),
     new \Doctrine\ODM\MongoDB\Tools\Console\Command\GenerateDocumentsCommand(),
@@ -17,9 +21,5 @@ $app->addCommands(array(
     new \Doctrine\ODM\MongoDB\Tools\Console\Command\Schema\DropCommand(),
     new \Doctrine\ODM\MongoDB\Tools\Console\Command\Schema\UpdateCommand(),
 ));
-
-if (isset($helperSet)) {
-    $app->setHelperSet($helperSet);
-}
 
 $app->run();