47a5f0e2d2d184d08e71d53a01dcc2c709efa2e6
Jeremy Mikola <jmikola@gmail.com>
2014-08-28 14:26:37 -0400

    Fix remove cascade through uninitialized Proxy objects
    Corresponding ORM fix: doctrine/doctrine2@acaf08d4b751f1fc9772de5c0e0c5d8371722f17
    
    See: http://www.doctrine-project.org/jira/browse/DDC-1193

---
+++ tests/Doctrine/ODM/MongoDB/Tests/Functional/Ticket/GH936Test.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Doctrine\ODM\MongoDB\Tests\Functional\Ticket;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ODM\MongoDB\Events;
+use Doctrine\ODM\MongoDB\Event\LifecycleEventArgs;
+use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
+
+class GH936Test extends \Doctrine\ODM\MongoDB\Tests\BaseTest
+{
+    public function testRemoveCascadesThroughProxyDocuments()
+    {
+        $listener = new GH936Listener();
+        $this->dm->getEventManager()->addEventListener(Events::postRemove, $listener);
+
+        $baz = new GH936Document();
+        $bar = new GH936Document($baz);
+        $foo = new GH936Document($bar);
+
+        $this->dm->persist($foo);
+        $this->dm->flush();
+        $this->dm->clear();
+
+        $foo = $this->dm->find(GH936Document::CLASSNAME, $foo->id);
+
+        $this->assertInstanceOf('Doctrine\ODM\MongoDB\Proxy\Proxy', $foo->ref);
+
+        $this->dm->remove($foo);
+        $this->dm->flush();
+
+        $this->assertCount(3, $listener->removed);
+        $this->assertNull($this->dm->find(GH936Document::CLASSNAME, $foo->id));
+        $this->assertNull($this->dm->find(GH936Document::CLASSNAME, $bar->id));
+        $this->assertNull($this->dm->find(GH936Document::CLASSNAME, $baz->id));
+    }
+}
+
+/** @ODM\Document */
+class GH936Document
+{
+    const CLASSNAME = __CLASS__;
+
+    /** @ODM\Id */
+    public $id;
+
+    /** @ODM\ReferenceOne(targetDocument="GH936Document", cascade={"persist","remove"}) */
+    public $ref;
+
+    public function __construct($ref = null)
+    {
+        $this->ref = $ref;
+    }
+}
+
+class GH936Listener
+{
+    public $removed = array();
+
+    public function postRemove(LifecycleEventArgs $args)
+    {
+        $this->removed[] = $args->getDocument();
+    }
+}
