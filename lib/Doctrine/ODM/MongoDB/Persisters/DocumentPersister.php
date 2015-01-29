e108ffedc30bc60204b9c9d573ee5728e896295e
Vitaly Chirkov <vitaly.chirkov@gmail.com>
2015-01-29 19:27:32 +0300

    DocumentPersister now uses shard key criteria

--- lib/Doctrine/ODM/MongoDB/Persisters/DocumentPersister.php
+++ lib/Doctrine/ODM/MongoDB/Persisters/DocumentPersister.php
@@ -30,6 +30,7 @@ use Doctrine\ODM\MongoDB\Hydrator\HydratorFactory;
 use Doctrine\ODM\MongoDB\LockException;
 use Doctrine\ODM\MongoDB\LockMode;
 use Doctrine\ODM\MongoDB\Mapping\ClassMetadata;
+use Doctrine\ODM\MongoDB\MongoDBException;
 use Doctrine\ODM\MongoDB\PersistentCollection\PersistentCollectionInterface;
 use Doctrine\ODM\MongoDB\Proxy\Proxy;
 use Doctrine\ODM\MongoDB\Query\CriteriaMerger;
@@ -301,7 +302,8 @@ class DocumentPersister
             }
 
             try {
-                $this->executeUpsert($data, $options);
+                $shardKeyQueryPart = $this->getShardKeyQuery($document, $options);
+                $this->executeUpsert($data, $options, $shardKeyQueryPart);
                 $this->handleCollections($document, $options);
                 unset($this->queuedUpserts[$oid]);
             } catch (\MongoException $e) {
@@ -312,16 +314,20 @@ class DocumentPersister
     }
 
     /**
-     * Executes a single upsert in {@link executeInserts}
+     * Executes a single upsert in {@link executeUpserts}
      *
      * @param array $data
      * @param array $options
+     * @param array $shardKeyQueryPart
      */
-    private function executeUpsert(array $data, array $options)
+    private function executeUpsert(array $data, array $options, array $shardKeyQueryPart = array())
     {
         $options['upsert'] = true;
-        $criteria = array('_id' => $data['$set']['_id']);
-        unset($data['$set']['_id']);
+        $criteria = array_merge(array('_id' => $data['$set']['_id']), $shardKeyQueryPart);
+
+        foreach (array_keys($criteria) as $field) {
+            unset($data['$set'][$field]);
+        }
 
         // Do not send an empty $set modifier
         if (empty($data['$set'])) {
@@ -371,7 +377,8 @@ class DocumentPersister
         $update = $this->pb->prepareUpdateData($document);
 
         $id = $this->class->getDatabaseIdentifierValue($id);
-        $query = array('_id' => $id);
+        $shardKeyQueryPart = $this->getShardKeyQuery($document, $options);
+        $query = array_merge(array('_id' => $id), $shardKeyQueryPart);
 
         // Include versioning logic to set the new version value in the database
         // and to ensure the version has not changed since this document object instance
@@ -403,6 +410,10 @@ class DocumentPersister
                 } else {
                     $query[$lockMapping['name']] = array('$exists' => false);
                 }
+            }
+
+            foreach (array_keys($query) as $field) {
+                unset($update['$set'][$field]);
             }
 
             $result = $this->collection->update($query, $update, $options);
@@ -526,6 +537,35 @@ class DocumentPersister
         }
 
         return $cursor;
+    }
+
+    /**
+     * @param object $document
+     * @param array $options
+     *
+     * @return array
+     * @throws MongoDBException
+     */
+    public function getShardKeyQuery($document, array $options = array())
+    {
+        if ( ! $this->class->isSharded()) {
+            return array();
+        }
+
+        $shardKey = $this->class->getShardKey();
+        $shardKeyFields = array_keys($shardKey['fields']);
+
+        $data = $this->uow->getDocumentActualData($document);
+
+        $shardKeyQueryPart = array();
+        foreach ($shardKeyFields as $field) {
+            $this->guardShardKeyFieldInvariants($document, $options, $field, $data);
+            $mapping = $this->class->fieldMappings[$field];
+            $value = Type::getType($mapping['type'])->convertToDatabaseValue($data[$field]);
+            $shardKeyQueryPart[$mapping['name']] = $value;
+        }
+
+        return $shardKeyQueryPart;
     }
 
     /**
@@ -1279,6 +1319,35 @@ class DocumentPersister
         // Take new snapshots from visited collections
         foreach ($this->uow->getVisitedCollections($document) as $coll) {
             $coll->takeSnapshot();
+        }
+    }
+
+    /**
+     * If the document is new, we can ignore shard key field value, otherwise throw an exception.
+     * Also shard key field should be presented in actual document data.
+     *
+     * @param object $document
+     * @param array  $options
+     * @param string $shardKeyField
+     * @param array  $actualDocumentData
+     *
+     * @throws MongoDBException
+     */
+    private function guardShardKeyFieldInvariants($document, array $options, $shardKeyField, $actualDocumentData)
+    {
+        $dcs = $this->uow->getDocumentChangeSet($document);
+        $isUpdate = $this->uow->isScheduledForUpdate($document);
+
+        if ($isUpdate
+            && isset($dcs[$shardKeyField])
+            && $dcs[$shardKeyField][0] != $dcs[$shardKeyField][1]
+            && (!isset($options['upsert']) || $options['upsert'] === true)
+        ) {
+            throw MongoDBException::shardKeyFieldCannotBeChanged($shardKeyField, $this->class->getName());
+        }
+
+        if (!isset($actualDocumentData[$shardKeyField])) {
+            throw MongoDBException::shardKeyFieldMissing($shardKeyField, $this->class->getName());
         }
     }
 }
---
+++ tests/Doctrine/ODM/MongoDB/Tests/Persisters/DocumentPersisterGetShardKeyQueryTest.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace Doctrine\ODM\MongoDB\Tests\Persisters;
+
+use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
+use Doctrine\ODM\MongoDB\Persisters\DocumentPersister;
+use Doctrine\ODM\MongoDB\Tests\BaseTest;
+
+class DocumentPersisterGetShardKeyQueryTest extends BaseTest
+{
+    public function testGetShardKeyQueryScalars()
+    {
+        $o = new ShardedByScalars();
+        $o->int = 1;
+        $o->string = 'hi';
+        $o->bool = true;
+        $o->float = 1.2;
+
+        /** @var DocumentPersister $persister */
+        $persister = $this->uow->getDocumentPersister(get_class($o));
+
+        $this->assertSame(
+            array('int' => $o->int, 'string' => $o->string, 'bool' => $o->bool, 'float' => $o->float),
+            $persister->getShardKeyQuery($o)
+        );
+    }
+
+    public function testGetShardKeyQueryObjects()
+    {
+        $o = new ShardedByObjects();
+        $o->oid = '54ca2c4c81fec698130041a7';
+        $o->bin = 'hi';
+        $o->date = new \DateTime();
+
+        /** @var DocumentPersister $persister */
+        $persister = $this->uow->getDocumentPersister(get_class($o));
+
+        $shardKeyQuery = $persister->getShardKeyQuery($o);
+
+        $this->assertInstanceOf('MongoId', $shardKeyQuery['oid']);
+        $this->assertSame($o->oid, $shardKeyQuery['oid']->{'$id'});
+
+        $this->assertInstanceOf('MongoBinData', $shardKeyQuery['bin']);
+        $this->assertSame($o->bin, $shardKeyQuery['bin']->bin);
+
+        $this->assertInstanceOf('MongoDate', $shardKeyQuery['date']);
+        $this->assertSame($o->date->getTimestamp(), $shardKeyQuery['date']->sec);
+        $this->assertSame(0, $shardKeyQuery['date']->usec);
+    }
+
+    public function testShardById()
+    {
+        $o = new ShardedById();
+        $o->identifier = new \MongoId();
+
+        /** @var DocumentPersister $persister */
+        $persister = $this->uow->getDocumentPersister(get_class($o));
+        $shardKeyQuery = $persister->getShardKeyQuery($o);
+
+        $this->assertSame(array('_id' => $o->identifier), $shardKeyQuery);
+    }
+}
+
+/**
+ * @ODM\Document
+ * @ODM\ShardKey(fields={"int"="asc","string"="asc","bool"="asc","float"="asc"})
+ */
+class ShardedByScalars
+{
+    /** @ODM\Id */
+    public $id;
+
+    /** @ODM\Int */
+    public $int;
+
+    /** @ODM\String */
+    public $string;
+
+    /** @ODM\Boolean */
+    public $bool;
+
+    /** @ODM\Float */
+    public $float;
+}
+
+/**
+ * @ODM\Document
+ * @ODM\ShardKey(fields={"oid"="asc","bin"="asc","date"="asc"})
+ */
+class ShardedByObjects
+{
+    /** @ODM\Id */
+    public $id;
+
+    /** @ODM\ObjectId */
+    public $oid;
+
+    /** @ODM\Bin */
+    public $bin;
+
+    /** @ODM\Date */
+    public $date;
+}
+
+/**
+ * @ODM\Document
+ * @ODM\ShardKey(fields={"identifier"="ask"})
+ */
+class ShardedById
+{
+    /** @ODM\Id */
+    public $identifier;
+}
