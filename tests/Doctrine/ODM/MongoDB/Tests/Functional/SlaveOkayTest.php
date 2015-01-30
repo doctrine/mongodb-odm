2a07fc0d728dd95f591484e5437aa72de5a34c71
Vitaly Chirkov <vitaly.chirkov@gmail.com>
2015-01-30 12:06:32 +0300

    Workaround replication lag in tests

--- tests/Doctrine/ODM/MongoDB/Tests/Functional/SlaveOkayTest.php
+++ tests/Doctrine/ODM/MongoDB/Tests/Functional/SlaveOkayTest.php
@@ -48,6 +48,9 @@ class SlaveOkayTest extends \Doctrine\ODM\MongoDB\Tests\BaseTest
 
         $this->assertSlaveOkayHint($slaveOkay, $cursor->getHints());
 
+        // Workaround replication lag
+        usleep(1000);
+
         $user = $cursor->getSingleResult();
 
         $this->assertInstanceOf('Doctrine\ODM\MongoDB\PersistentCollection', $user->getGroups());
@@ -67,6 +70,9 @@ class SlaveOkayTest extends \Doctrine\ODM\MongoDB\Tests\BaseTest
         $cursor->setHints(array(Query::HINT_SLAVE_OKAY => $slaveOkay));
 
         $this->assertSlaveOkayHint($slaveOkay, $cursor->getHints());
+
+        // Workaround replication lag
+        usleep(1000);
 
         $user = $cursor->getSingleResult();
 
