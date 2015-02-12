7f655cd77e5c7a180aeb809b73033f78ee064194
Gerry Vandermaesen <gerryvdm@me.com>
2015-02-12 13:20:28 +0100

    Added test

---
+++ tests/Doctrine/ODM/MongoDB/Tests/Types/DateTypeTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Doctrine\ODM\MongoDB\Tests\Types;
+
+use Doctrine\ODM\MongoDB\Types\Type;
+
+class DateTypeTest extends \PHPUnit_Framework_TestCase
+{
+    public function testConvertToDatabaseValue()
+    {
+        $type = Type::getType('date');
+
+        $date = \DateTime::createFromFormat('U.u', '1423743340.626000'); // "seconds.microseconds"
+
+        $this->assertNull($type->convertToDatabaseValue(null), 'null is not converted');
+        $this->assertEquals(1423743340, $type->convertToDatabaseValue($time)->sec);
+        $this->assertEquals(626000, $type->convertToDatabaseValue($time)->usec));
+    }
+
+    public function testConvertToPHPValue()
+    {
+        $type = Type::getType('date');
+
+        $date = new \MongoDate(1423743340, 626000);
+
+        $this->assertNull($type->convertToPHPValue(null), 'null is not converted');
+        $this->assertEquals('1423743340.626000', $type->convertToPHPValue($date)->format('U.u'));
+    }
+}
