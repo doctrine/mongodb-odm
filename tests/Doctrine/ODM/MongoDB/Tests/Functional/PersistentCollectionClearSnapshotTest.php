f6676740c6a5847198e76396c7a154b105865d74
Sebastian Zorn <s.zorn@voycer.com>
2015-02-25 17:53:07 +0100

    Remove extra check if collection is cleared to get correct deleteDiff.

--- tests/Doctrine/ODM/MongoDB/Tests/Functional/PersistentCollectionClearSnapshotTest.php
+++ tests/Doctrine/ODM/MongoDB/Tests/Functional/PersistentCollectionClearSnapshotTest.php
@@ -11,6 +11,8 @@ class PersistentCollectionClearSnapshotTest extends \Doctrine\ODM\MongoDB\Tests\
 {
     private $project;
 
+    private $projectTwo;
+
     private $developer;
 
     public function setUp()
@@ -18,16 +20,19 @@ class PersistentCollectionClearSnapshotTest extends \Doctrine\ODM\MongoDB\Tests\
         parent::setUp();
 
         $project = new Project('project x');
+        $project2 = new Project('secondProject');
 
         $developer = new Developer('Martha Facka');
         $developer->getProjects()->add($project);
 
         $this->dm->persist($project);
         $this->dm->persist($developer);
+        $this->dm->persist($project2);
         $this->dm->flush();
         $this->dm->clear();
 
         $this->project = $this->dm->find(get_class($project), $project->getId());
+        $this->projectTwo = $this->dm->find(get_class($project2), $project2->getId());
         $this->developer = $this->dm->find(get_class($developer), $developer->getId());
     }
 
@@ -40,5 +45,11 @@ class PersistentCollectionClearSnapshotTest extends \Doctrine\ODM\MongoDB\Tests\
         $this->assertTrue($this->developer->getProjects()->isCleared());
         $this->assertTrue($this->developer->getProjects()->isInitialized());
         $this->assertSame($this->developer->getProjects()->getSnapshot()[0], $this->project);
+        $this->assertSame($this->developer->getProjects()->getDeleteDiff()[0], $this->project);
+
+        $this->developer->getProjects()->add($this->projectTwo);
+
+        $this->assertNotNull($this->developer->getProjects()->getInsertDiff());
+        $this->assertSame($this->developer->getProjects()->getInsertDiff()[0], $this->projectTwo);
     }
 }
