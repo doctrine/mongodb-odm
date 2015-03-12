e674185e6d03175bcb26c9cd7af555fd2f47e37c
Maciej Malarz <malarzm@gmail.com>
2015-03-12 20:01:28 +0100

    Fixed case with deeply embedded documents

--- tests/Doctrine/ODM/MongoDB/Tests/Functional/VersionTest.php
+++ tests/Doctrine/ODM/MongoDB/Tests/Functional/VersionTest.php
@@ -8,29 +8,34 @@ class VersionTest extends \Doctrine\ODM\MongoDB\Tests\BaseTest
 {
     public function testVersioningWhenManipulatingEmbedMany()
     {
+        $expectedVersion = 1;
         $doc = new VersionedDocument();
         $doc->name = 'test';
         $doc->embedMany[] = new VersionedEmbeddedDocument('embed 1');
         $doc->embedMany[] = new VersionedEmbeddedDocument('embed 2');
         $this->dm->persist($doc);
         $this->dm->flush();
-        $this->assertEquals(1, $doc->version);
+        $this->assertEquals($expectedVersion++, $doc->version);
 
         $doc->embedMany[] = new VersionedEmbeddedDocument('embed 3');
         $this->dm->flush();
-        $this->assertEquals(2, $doc->version);
+        $this->assertEquals($expectedVersion++, $doc->version);
+        
+        $doc->embedMany[0]->embedMany[] = new VersionedEmbeddedDocument('deeply embed 1');
+        $this->dm->flush();
+        $this->assertEquals($expectedVersion++, $doc->version);
         
         unset($doc->embedMany[1]);
         $this->dm->flush();
-        $this->assertEquals(3, $doc->version);
+        $this->assertEquals($expectedVersion++, $doc->version);
 
         $doc->embedMany->clear();
         $this->dm->flush();
-        $this->assertEquals(4, $doc->version); 
+        $this->assertEquals($expectedVersion++, $doc->version); 
         
         $doc->embedMany = null;
         $this->dm->flush();
-        $this->assertEquals(5, $doc->version); 
+        $this->assertEquals($expectedVersion++, $doc->version);
     }
 }
 
@@ -62,10 +67,15 @@ class VersionedDocument
  */
 class VersionedEmbeddedDocument
 {
+    /** @ODM\String */
     public $value;
+    
+    /** @ODM\EmbedMany(targetDocument="VersionedEmbeddedDocument") */
+    public $embedMany;
     
     public function __construct($value) 
     {
         $this->value = $value;
+        $this->embedMany = new \Doctrine\Common\Collections\ArrayCollection();
     }
 }
