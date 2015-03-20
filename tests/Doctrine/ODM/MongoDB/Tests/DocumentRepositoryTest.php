f8bbc1389402c2617cbf0a6fc8842f2e281f59b8
Maciej Malarz <malarzm@gmail.com>
2015-03-20 10:15:23 +0100

    Failing test case for https://github.com/symfony/symfony/issues/13961

--- tests/Doctrine/ODM/MongoDB/Tests/DocumentRepositoryTest.php
+++ tests/Doctrine/ODM/MongoDB/Tests/DocumentRepositoryTest.php
@@ -17,4 +17,18 @@ class DocumentRepositoryTest extends BaseTest
         $this->assertNull($criteria->getWhereExpression());
         $this->assertInstanceOf('Doctrine\Common\Collections\Collection', $repository->matching($criteria));
     }
+    
+    public function testFindByManyIds()
+    {
+        $t1 = new \Documents\Task('test');;
+        $this->dm->persist($t1);
+        $this->dm->flush();
+        $r = $this->dm->getRepository('Documents\Task');
+        
+        $results = $r->findBy(array('id' => $t1->getId()));
+        $this->assertCount(1, $results);
+        
+        $results = $r->findBy(array('id' => array($t1->getId())));
+        $this->assertCount(1, $results);
+    }
 }
