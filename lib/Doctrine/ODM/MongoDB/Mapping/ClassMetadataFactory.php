9a8b74c9195b6c489f88dd9fa7816bf35ba99eb3
Maciej Malarz <malarzm@gmail.com>
2015-06-09 23:37:02 +0200

    Fix overriding id strategy

--- lib/Doctrine/ODM/MongoDB/Mapping/ClassMetadataFactory.php
+++ lib/Doctrine/ODM/MongoDB/Mapping/ClassMetadataFactory.php
@@ -159,7 +159,7 @@ class ClassMetadataFactory extends AbstractClassMetadataFactory
 
         $this->validateIdentifier($class);
 
-        if ($parent && $rootEntityFound) {
+        if ($parent && $rootEntityFound && $parent->generatorType === $class->generatorType) {
             if ($parent->generatorType) {
                 $class->setIdGeneratorType($parent->generatorType);
             }
