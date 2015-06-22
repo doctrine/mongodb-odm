2935e58476047d14c7c7d7516130359423cb81c2
Jonathan Block <jblock@lyft.com>
2015-06-21 17:42:45 -0700

    Rename test

--- tests/Doctrine/ODM/MongoDB/Tests/Functional/Ticket/GH1156Test.php
+++ tests/Doctrine/ODM/MongoDB/Tests/Functional/Ticket/GH1156Test.php
@@ -5,42 +5,42 @@ namespace Doctrine\ODM\MongoDB\Tests\Functional\Ticket;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
 
-class GHXXXXTest extends \Doctrine\ODM\MongoDB\Tests\BaseTest
+class GH1156Test extends \Doctrine\ODM\MongoDB\Tests\BaseTest
 {
     public function testReplacementOfIdentifiedEmbedManyElements()
     {
-        $book = new GHXXXXBook();
-        $book->chapters->add(new GHXXXXChapter('A'));
+        $book = new GH1156Book();
+        $book->chapters->add(new GH1156Chapter('A'));
 
         $this->dm->persist($book);
         $this->dm->flush();
         $this->dm->clear();
 
-        $book = $this->dm->getRepository(GHXXXXBook::CLASSNAME)->findOneBy(array('_id' => $book->id));
+        $book = $this->dm->getRepository(GH1156Book::CLASSNAME)->findOneBy(array('_id' => $book->id));
         $firstChapter = $book->chapters->first();
         $firstChapter->name = "Apple";
 
         // Add some pages.
-        $firstChapter->pages->add(new GHXXXXPage(1));
-        $firstChapter->pages->add(new GHXXXXPage(2));
+        $firstChapter->pages->add(new GH1156Page(1));
+        $firstChapter->pages->add(new GH1156Page(2));
 
         $this->dm->flush();
         $this->dm->clear();
 
-        $book = $this->dm->getRepository(GHXXXXBook::CLASSNAME)->findOneBy(array('_id' => $book->id));
+        $book = $this->dm->getRepository(GH1156Book::CLASSNAME)->findOneBy(array('_id' => $book->id));
         $this->assertEquals(2, $book->chapters->first()->pages->count());
     }
 }
 
 /** @ODM\Document */
-class GHXXXXBook
+class GH1156Book
 {
     const CLASSNAME = __CLASS__;
 
     /** @ODM\Id */
     public $id;
 
-    /** @ODM\EmbedMany(targetDocument="GHXXXXChapter", strategy="atomicSet") */
+    /** @ODM\EmbedMany(targetDocument="GH1156Chapter", strategy="atomicSet") */
     public $chapters;
 
     public function __construct()
@@ -50,12 +50,12 @@ class GHXXXXBook
 }
 
 /** @ODM\EmbeddedDocument */
-class GHXXXXChapter
+class GH1156Chapter
 {
     /** @ODM\String */
     public $name;
 
-    /** @ODM\EmbedMany(targetDocument="GHXXXXPage") */
+    /** @ODM\EmbedMany(targetDocument="GH1156Page") */
     public $pages;
 
     public function __construct($name)
@@ -65,7 +65,7 @@ class GHXXXXChapter
 }
 
 /** @ODM\EmbeddedDocument */
-class GHXXXXPage
+class GH1156Page
 {
     /** @ODM\Id */
     public $id;
