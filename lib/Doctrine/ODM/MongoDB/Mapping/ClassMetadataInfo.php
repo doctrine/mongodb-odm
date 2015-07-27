033aff2a9b327385e19bbc7bba05833b7e993175
Marcos Passos <marcos@marcospassos.com>
2015-07-27 17:21:29 -0300

    Add support for custom object ids (#1178)
    Fix file location
    
    Fix CS issues
    Add type mismatch test

--- lib/Doctrine/ODM/MongoDB/Mapping/ClassMetadataInfo.php
+++ lib/Doctrine/ODM/MongoDB/Mapping/ClassMetadataInfo.php
@@ -1137,7 +1137,7 @@ class ClassMetadataInfo implements \Doctrine\Common\Persistence\Mapping\ClassMet
                     } elseif ($generatorType === ClassMetadata::GENERATOR_TYPE_INCREMENT) {
                         $mapping['type'] = 'int_id';
                     } else {
-                        $mapping['type'] = 'custom_id';
+                        $mapping['type'] = $mapping['type']  == 'id' ? 'custom_id' : $mapping['type'];
                     }
 
                     unset($this->generatorOptions['type']);
