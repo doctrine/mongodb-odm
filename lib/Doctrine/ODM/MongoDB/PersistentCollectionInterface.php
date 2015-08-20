17088ba71927639b504c5db3fb98047e34ec16f1
Maciej Malarz <malarzm@gmail.com>
2015-08-20 17:05:36 +0200

    Introduce PersistentCollectionInterface

---
+++ lib/Doctrine/ODM/MongoDB/PersistentCollectionInterface.php
@@ -0,0 +1,174 @@
+<?php
+/*
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license. For more information, see
+ * <http://www.doctrine-project.org>.
+ */
+
+namespace Doctrine\ODM\MongoDB;
+
+use Doctrine\Common\Collections\Collection as BaseCollection;
+use Doctrine\Common\Persistence\Mapping\ClassMetadata;
+use Doctrine\ODM\MongoDB\DocumentManager;
+use Doctrine\ODM\MongoDB\MongoDBException;
+
+interface PersistentCollectionInterface extends BaseCollection
+{
+    /**
+     * Sets the document manager and unit of work (used during merge operations).
+     *
+     * @param DocumentManager $dm
+     */
+    public function setDocumentManager(DocumentManager $dm);
+
+    /**
+     * Sets the array of raw mongo data that will be used to initialize this collection.
+     *
+     * @param array $mongoData
+     */
+    public function setMongoData(array $mongoData);
+
+    /**
+     * Gets the array of raw mongo data that will be used to initialize this collection.
+     *
+     * @return array $mongoData
+     */
+    public function getMongoData();
+
+    /**
+     * Set hints to account for during reconstitution/lookup of the documents.
+     *
+     * @param array $hints
+     */
+    public function setHints(array $hints);
+
+    /**
+     * Get hints to account for during reconstitution/lookup of the documents.
+     *
+     * @return array $hints
+     */
+    public function getHints();
+
+    /**
+     * Initializes the collection by loading its contents from the database
+     * if the collection is not yet initialized.
+     */
+    public function initialize();
+
+    /**
+     * Gets a boolean flag indicating whether this collection is dirty which means
+     * its state needs to be synchronized with the database.
+     *
+     * @return boolean TRUE if the collection is dirty, FALSE otherwise.
+     */
+    public function isDirty();
+
+    /**
+     * Sets a boolean flag, indicating whether this collection is dirty.
+     *
+     * @param boolean $dirty Whether the collection should be marked dirty or not.
+     */
+    public function setDirty($dirty);
+
+    /**
+     * INTERNAL:
+     * Sets the collection's owning entity together with the AssociationMapping that
+     * describes the association between the owner and the elements of the collection.
+     *
+     * @param object $document
+     * @param array $mapping
+     */
+    public function setOwner($document, array $mapping);
+
+    /**
+     * INTERNAL:
+     * Tells this collection to take a snapshot of its current state reindexing
+     * itself numerically if using save strategy that is enforcing BSON array.
+     * Reindexing is safe as snapshot is taken only after synchronizing collection
+     * with database or clearing it.
+     */
+    public function takeSnapshot();
+
+    /**
+     * INTERNAL:
+     * Clears the internal snapshot information and sets isDirty to true if the collection
+     * has elements.
+     */
+    public function clearSnapshot();
+
+    /**
+     * INTERNAL:
+     * Returns the last snapshot of the elements in the collection.
+     *
+     * @return array The last snapshot of the elements.
+     */
+    public function getSnapshot();
+
+    /**
+     * INTERNAL:
+     * getDeleteDiff
+     *
+     * @return array
+     */
+    public function getDeleteDiff();
+
+    /**
+     * INTERNAL: get objects that were removed, unlike getDeleteDiff this doesn't care about indices.
+     *
+     * @return array
+     */
+    public function getDeletedDocuments();
+
+    /**
+     * INTERNAL:
+     * getInsertDiff
+     *
+     * @return array
+     */
+    public function getInsertDiff();
+
+    /**
+     * INTERNAL:
+     * Gets the collection owner.
+     *
+     * @return object
+     */
+    public function getOwner();
+
+    /**
+     * @return array
+     */
+    public function getMapping();
+
+    /**
+     * @return ClassMetadata
+     * @throws MongoDBException
+     */
+    public function getTypeClass();
+
+    /**
+     * Sets the initialized flag of the collection, forcing it into that state.
+     *
+     * @param boolean $bool
+     */
+    public function setInitialized($bool);
+
+    /**
+     * Checks whether this collection has been initialized.
+     *
+     * @return boolean
+     */
+    public function isInitialized();
+}
