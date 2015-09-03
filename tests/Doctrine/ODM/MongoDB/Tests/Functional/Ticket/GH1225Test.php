3f786ba260c907f27a499278575b58b3f5a7b60f
Jonathan Block <jblock@lyft.com>
2015-09-03 16:27:28 -0700

    Small cleanup.

--- tests/Doctrine/ODM/MongoDB/Tests/Functional/Ticket/GH1225Test.php
+++ tests/Doctrine/ODM/MongoDB/Tests/Functional/Ticket/GH1225Test.php
@@ -10,16 +10,15 @@ class GH1225Test extends \Doctrine\ODM\MongoDB\Tests\BaseTest
     public function testAddOnUninitializedCollection()
     {
         $doc = new GH1225Document();
-        $mySubDoc = new GH1225EmbeddedDocument('foo');
-        $doc->embeds->add($mySubDoc);
+        $doc->embeds->add(new GH1225EmbeddedDocument('foo'));
         $this->dm->persist($doc);
         $this->dm->flush();
         $this->dm->clear();
 
         $doc = $this->dm->getRepository(get_class($doc))->find($doc->id);
-        $removedEmbeddedDocumentOne = $doc->embeds->first();
+        $embeddedDoc = $doc->embeds->first();
         $doc->embeds->clear();
-        $doc->embeds->add($removedEmbeddedDocumentOne);
+        $doc->embeds->add($embeddedDoc);
         $this->dm->flush();
         $this->dm->clear();
 
