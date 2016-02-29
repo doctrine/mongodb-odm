622682bd770e08cc643ed91c06aae812190673f0
Bilge <bilge@scriptfusion.com>
2016-02-29 22:07:34 +0000

    Readded redundant import for GH1299User.

--- tests/Doctrine/ODM/MongoDB/Tests/Tools/GH1299/GH1299User.php
+++ tests/Doctrine/ODM/MongoDB/Tests/Tools/GH1299/GH1299User.php
@@ -3,6 +3,7 @@
 namespace Doctrine\ODM\MongoDB\Tests\Tools\GH1299;
 
 use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
+use Doctrine\ODM\MongoDB\Tests\Tools\GH1299\BaseUser;
 
 /** @ODM\Document */
 class GH1299User extends BaseUser
