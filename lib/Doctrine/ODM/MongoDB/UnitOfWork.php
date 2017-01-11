d561d0edc693ea81dcc3fe1036fffa2711296ab9
Andreas Braun <git@alcaeus.org>
2017-01-11 11:49:41 +0100

    Check for valid MongoID

--- lib/Doctrine/ODM/MongoDB/UnitOfWork.php
+++ lib/Doctrine/ODM/MongoDB/UnitOfWork.php
@@ -1096,8 +1096,7 @@ class UnitOfWork implements PropertyChangedListener
                 ));
             }
 
-            // @todo NG: Replace MongoId::isValid
-            if (false && $class->generatorType === ClassMetadata::GENERATOR_TYPE_AUTO && $idValue !== null) {// && ! \MongoId::isValid($idValue)) {
+            if ($class->generatorType === ClassMetadata::GENERATOR_TYPE_AUTO && $idValue !== null && ! preg_match('#^[0-9a-f]{24}$#', $idValue)) {
                 throw new \InvalidArgumentException(sprintf(
                     '%s uses AUTO identifier generation strategy but provided identifier is not a valid ObjectId.',
                     get_class($document)
