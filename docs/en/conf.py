056d89c393cceeda3107fac5af4a73b2e7fff5bc
Maciej Malarz <malarzm@gmail.com>
2018-01-18 13:14:16 +0100

    Fix version to be string again

--- docs/en/conf.py
+++ docs/en/conf.py
@@ -45,7 +45,7 @@ copyright = u'2013, Doctrine Project Team'
 # built documents.
 #
 # The short X.Y version.
-version = 2.0
+version = '2.0'
 # The full version, including alpha/beta/rc tags.
 release = '2.0.0-pre'
 
