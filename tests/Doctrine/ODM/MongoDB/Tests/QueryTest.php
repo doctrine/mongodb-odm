e2755f39874e2c1b8f57ce857aa4346a9b1a65a5
Maciej Malarz <malarzm@gmail.com>
2018-05-17 13:39:25 +0200

    Fix querying fields in reference structures

--- tests/Doctrine/ODM/MongoDB/Tests/QueryTest.php
+++ tests/Doctrine/ODM/MongoDB/Tests/QueryTest.php
@@ -299,8 +299,22 @@ class QueryTest extends BaseTest
         $query = $qb->getQuery();
         $debug = $query->debug('query');
 
-        $this->assertArrayHasKey('eO.eO.e1.eO.eP.pO._id', $debug);
-        $this->assertEquals($identifier, $debug['eO.eO.e1.eO.eP.pO._id']);
+        $this->assertArrayHasKey('eO.eO.e1.eO.eP.pO.$id', $debug);
+        $this->assertEquals($identifier, $debug['eO.eO.e1.eO.eP.pO.$id']);
+    }
+
+    public function testQueryWithMultipleEmbeddedDocumentsAndReferenceUsingDollarSign()
+    {
+        $identifier = new ObjectId();
+
+        $qb = $this->dm->createQueryBuilder(__NAMESPACE__ . '\EmbedTest')
+            ->find()
+            ->field('embeddedOne.embeddedOne.embeddedMany.embeddedOne.pet.owner.$id')->equals((string) $identifier);
+        $query = $qb->getQuery();
+        $debug = $query->debug('query');
+
+        $this->assertArrayHasKey('eO.eO.e1.eO.eP.pO.$id', $debug);
+        $this->assertEquals($identifier, $debug['eO.eO.e1.eO.eP.pO.$id']);
     }
 
     public function testSelectVsSingleCollectionInheritance()
