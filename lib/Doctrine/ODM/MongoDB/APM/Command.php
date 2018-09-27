5e1639c21aaf9e213a01ef3d20c8f8fd3cfae327
Andreas Braun <git@alcaeus.org>
2018-09-27 08:08:52 +0200

    Replace QueryLogger in tests with CommandLogger

---
+++ lib/Doctrine/ODM/MongoDB/APM/Command.php
@@ -0,0 +1,91 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Doctrine\ODM\MongoDB\APM;
+
+use LogicException;
+use MongoDB\Driver\Monitoring\CommandFailedEvent;
+use MongoDB\Driver\Monitoring\CommandStartedEvent;
+use MongoDB\Driver\Monitoring\CommandSucceededEvent;
+use MongoDB\Driver\Server;
+
+final class Command
+{
+    /** @var CommandStartedEvent */
+    private $startedEvent;
+
+    /** @var CommandSucceededEvent|CommandFailedEvent */
+    private $finishedEvent;
+
+    private function __construct()
+    {
+    }
+
+    public static function createForSucceededCommand(CommandStartedEvent $startedEvent, CommandSucceededEvent $succeededEvent): self
+    {
+        self::checkRequestIds($startedEvent, $succeededEvent);
+
+        $instance = new self();
+        $instance->startedEvent = $startedEvent;
+        $instance->finishedEvent = $succeededEvent;
+
+        return $instance;
+    }
+
+    public static function createForFailedCommand(CommandStartedEvent $startedEvent, CommandFailedEvent $failedEvent): self
+    {
+        self::checkRequestIds($startedEvent, $failedEvent);
+
+        $instance = new self();
+        $instance->startedEvent = $startedEvent;
+        $instance->finishedEvent = $failedEvent;
+
+        return $instance;
+    }
+
+    /**
+     * @param CommandSucceededEvent|CommandFailedEvent $finishedEvent
+     */
+    private static function checkRequestIds(CommandStartedEvent $startedEvent, $finishedEvent): void
+    {
+        if ($startedEvent->getRequestId() !== $finishedEvent->getRequestId()) {
+            throw new LogicException('Cannot create APM command for events with different request IDs');
+        }
+    }
+
+    public function getCommandName(): string
+    {
+        return $this->startedEvent->getCommandName();
+    }
+
+    public function getCommand(): object
+    {
+        return $this->startedEvent->getCommand();
+    }
+
+    public function getDurationMicros(): int
+    {
+        return $this->finishedEvent->getDurationMicros();
+    }
+
+    public function getRequestId(): string
+    {
+        return $this->startedEvent->getRequestId();
+    }
+
+    public function getServer(): Server
+    {
+        return $this->finishedEvent->getServer();
+    }
+
+    public function getReply(): object
+    {
+        return $this->finishedEvent->getReply();
+    }
+
+    public function getError(): ?\Throwable
+    {
+        return $this->finishedEvent instanceof CommandFailedEvent ? $this->finishedEvent->getError() : null;
+    }
+}
---
+++ lib/Doctrine/ODM/MongoDB/APM/CommandLogger.php
@@ -0,0 +1,102 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Doctrine\ODM\MongoDB\APM;
+
+use Countable;
+use MongoDB\Driver\Monitoring\CommandFailedEvent;
+use MongoDB\Driver\Monitoring\CommandStartedEvent;
+use MongoDB\Driver\Monitoring\CommandSubscriber;
+use MongoDB\Driver\Monitoring\CommandSucceededEvent;
+use function count;
+use function MongoDB\Driver\Monitoring\addSubscriber;
+use function MongoDB\Driver\Monitoring\removeSubscriber;
+
+final class CommandLogger implements Countable, CommandSubscriber
+{
+    /** @var Command[] */
+    private $commands = [];
+
+    /** @var CommandStartedEvent[] */
+    private $startedCommands = [];
+
+    /** @var bool */
+    private $registered = false;
+
+    public function register(): void
+    {
+        if ($this->registered) {
+            return;
+        }
+
+        $this->registered = true;
+        addSubscriber($this);
+    }
+
+    public function unregister(): void
+    {
+        if (! $this->registered) {
+            return;
+        }
+
+        removeSubscriber($this);
+        $this->registered = false;
+    }
+
+    public function commandStarted(CommandStartedEvent $event)
+    {
+        $this->startedCommands[$event->getRequestId()] = $event;
+    }
+
+    public function commandSucceeded(CommandSucceededEvent $event)
+    {
+        $commandStartedEvent = $this->findAndRemoveCommandStartedEvent($event->getRequestId());
+        if (! $commandStartedEvent) {
+            return;
+        }
+
+        $this->logCommand(Command::createForSucceededCommand($commandStartedEvent, $event));
+    }
+
+    public function commandFailed(CommandFailedEvent $event)
+    {
+        $commandStartedEvent = $this->findAndRemoveCommandStartedEvent($event->getRequestId());
+        if (! $commandStartedEvent) {
+            return;
+        }
+
+        $this->logCommand(Command::createForFailedCommand($commandStartedEvent, $event));
+    }
+
+    public function clear(): void
+    {
+        $this->commands = [];
+    }
+
+    public function count(): int
+    {
+        return count($this->commands);
+    }
+
+    /**
+     * @return Command[]
+     */
+    public function getAll(): array
+    {
+        return $this->commands;
+    }
+
+    private function findAndRemoveCommandStartedEvent(string $requestId): ?CommandStartedEvent
+    {
+        $startedEvent = $this->startedCommands[$requestId] ?? null;
+        unset($this->startedCommands[$requestId]);
+
+        return $startedEvent;
+    }
+
+    private function logCommand(Command $command): void
+    {
+        $this->commands[] = $command;
+    }
+}
