52d47d3f98e3ffd8632b7cbb5f73ba36bf9d4979
Andreas Braun <git@alcaeus.org>
2019-03-06 07:12:09 +0100

    Add test with overly long index name to confirm exception

--- tests/Doctrine/ODM/MongoDB/Tests/Functional/Ticket/GH1344Test.php
+++ tests/Doctrine/ODM/MongoDB/Tests/Functional/Ticket/GH1344Test.php
@@ -4,6 +4,7 @@ namespace Doctrine\ODM\MongoDB\Tests\Functional\Ticket;
 
 use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
 use Doctrine\ODM\MongoDB\Tests\BaseTest;
+use MongoResultException;
 
 class GH1344Test extends BaseTest
 {
@@ -17,6 +18,16 @@ class GH1344Test extends BaseTest
         self::assertSame('embedded2_embedded_nested', $indexes[3]['options']['name']);
 
         $this->dm->getSchemaManager()->ensureDocumentIndexes(GH1344Main::class);
+    }
+
+    public function testGeneratingIndexesWithTooLongIndexNameThrowsException()
+    {
+        // Ensure that at least the beginning of the index name is contained in
+        // the exception message. This can vary between driver/server versions.
+        $this->expectException(MongoResultException::class);
+        $this->expectExceptionMessageRegExp('#GH1344TooLongIndexName.\$embedded1_this_is_a_really_long_name_that#');
+
+        $this->dm->getSchemaManager()->ensureDocumentIndexes(GH1344TooLongIndexName::class);
     }
 }
 
@@ -51,6 +62,26 @@ class GH1344Embedded
  * @ODM\Index(keys={"property"="asc"}, name="nested")
  */
 class GH1344EmbeddedNested
+{
+    /** @ODM\Field */
+    public $property;
+}
+
+/** @ODM\Document */
+class GH1344TooLongIndexName
+{
+    /** @ODM\Id */
+    public $id;
+
+    /** @ODM\EmbedOne(targetDocument=GH1344TooLongIndexNameEmbedded::class) */
+    public $embedded1;
+}
+
+/**
+ * @ODM\EmbeddedDocument
+ * @ODM\Index(keys={"property"="asc"}, name="this_is_a_really_long_name_that_will_cause_problems_for_whoever_tries_to_use_it_whether_in_an_embedded_field_or_not")
+ */
+class GH1344TooLongIndexNameEmbedded
 {
     /** @ODM\Field */
     public $property;
