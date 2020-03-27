dea6ccbbbfa2fe0557618c3897bea53bc0e6f3c8
josemiguelq <jose.miguelq95@gmail.com>
2020-03-27 12:24:11 -0300

    primary key

--- tests/Documents/Campaign.php
+++ tests/Documents/Campaign.php
@@ -10,6 +10,14 @@ use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
  */
 class Campaign
 {
+    /** @ODM\Id */
+    protected $id;
+
     /** @ODM\ReferenceMany(targetDocument="Promo\Documents\Webhook", storeAs="id", mappedBy="campaign")*/
     protected $referenceCampaigns;
+
+    public function getId()
+    {
+        return $this->id;
+    }
 }
--- tests/Documents/ReferenceCampaign.php
+++ tests/Documents/ReferenceCampaign.php
@@ -7,6 +7,9 @@ use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
 /** @ODM\Document */
 class ReferenceCampaign
 {
+    /** @ODM\Id */
+    protected $id;
+
     /**
      * @ODM\ReferenceOne(targetDocument="Promo\Documents\Campaign",
      *     storeAs="id", inversedBy="referenceCampaigns",
@@ -14,4 +17,9 @@ class ReferenceCampaign
      * )
      */
     protected $campaign;
+
+    public function getId()
+    {
+        return $this->id;
+    }
 }
