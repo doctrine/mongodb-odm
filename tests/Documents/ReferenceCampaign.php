4168a01ff1763ff3cdac2d7be5a8a9712d602ac1
josemiguelq <jose.miguelq95@gmail.com>
2020-03-27 12:33:28 -0300

    correct document

--- tests/Documents/ReferenceCampaign.php
+++ tests/Documents/ReferenceCampaign.php
@@ -11,7 +11,7 @@ class ReferenceCampaign
     protected $id;
 
     /**
-     * @ODM\ReferenceOne(targetDocument="Promo\Documents\Campaign",
+     * @ODM\ReferenceOne(targetDocument="Campaign",
      *     storeAs="id", inversedBy="referenceCampaigns",
      *     name="campaignId"
      * )
