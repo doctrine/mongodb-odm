201517edf09b9c4f30f3e9ad2b89405b151a1d60
Fran Moreno <franmomu@gmail.com>
2020-10-18 19:27:36 +0200

    Fix PHPDoc in DocumentPersister
    The phpdoc did not match the current return and arguments types.

--- lib/Doctrine/ODM/MongoDB/Persisters/DocumentPersister.php
+++ lib/Doctrine/ODM/MongoDB/Persisters/DocumentPersister.php
@@ -614,9 +614,9 @@ final class DocumentPersister
      * @param object $document The document object to fill, if any.
      * @param array  $hints    Hints for document creation.
      *
-     * @return object|null The filled and managed document object or NULL, if the query result is empty.
+     * @return object The filled and managed document object.
      */
-    private function createDocument(array $result, ?object $document = null, array $hints = []) : ?object
+    private function createDocument(array $result, ?object $document = null, array $hints = []) : object
     {
         if ($document !== null) {
             $hints[Query::HINT_REFRESH] = true;
@@ -885,7 +885,7 @@ final class DocumentPersister
     }
 
     /**
-     * @param int|string $sort
+     * @param int|string|null $sort
      *
      * @return int|string|null
      */
