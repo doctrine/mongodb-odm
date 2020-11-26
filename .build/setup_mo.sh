2e010c8cdb3b26d0bd1372b2c1ae19aeebef6284
Andreas Braun <git@alcaeus.org>
2020-11-26 14:15:57 +0100

    Move build files to generic directory

--- .build/setup_mo.sh
+++ .build/setup_mo.sh
@@ -5,28 +5,30 @@ echo Loading MO for $DEPLOYMENT
 mongo-orchestration --version
 
 if [[ -z $TRAVIS_BUILD_DIR ]]; then
-    export TRAVIS_BUILD_DIR=`pwd`;
+    export BUILD_DIR=`pwd`;
+else
+    export BUILD_DIR=$TRAVIS_BUILD_DIR;
 fi
 
 case $DEPLOYMENT in
   SHARDED_CLUSTER)
-    ${TRAVIS_BUILD_DIR}/.travis/mo.sh ${TRAVIS_BUILD_DIR}/.travis/configurations/sharded_clusters/cluster.json start > /tmp/mo-result.json
+    ${BUILD_DIR}/.build/mo.sh ${BUILD_DIR}/.build/configurations/sharded_clusters/cluster.json start > /tmp/mo-result.json
     cat /tmp/mo-result.json | tail -n 1 | php -r 'echo json_decode(file_get_contents("php://stdin"))->mongodb_uri, "/?retryWrites=false";' > /tmp/uri.txt
     ;;
   SHARDED_CLUSTER_RS)
-    ${TRAVIS_BUILD_DIR}/.travis/mo.sh ${TRAVIS_BUILD_DIR}/.travis/configurations/sharded_clusters/cluster_replset.json start > /tmp/mo-result.json
+    ${BUILD_DIR}/.build/mo.sh ${BUILD_DIR}/.build/configurations/sharded_clusters/cluster_replset.json start > /tmp/mo-result.json
     cat /tmp/mo-result.json | tail -n 1 | php -r 'echo json_decode(file_get_contents("php://stdin"))->mongodb_uri;' > /tmp/uri.txt
     ;;
   STANDALONE_AUTH)
-    ${TRAVIS_BUILD_DIR}/.travis/mo.sh ${TRAVIS_BUILD_DIR}/.travis/configurations/standalone/standalone-auth.json start > /tmp/mo-result.json
+    ${BUILD_DIR}/.build/mo.sh ${BUILD_DIR}/.build/configurations/standalone/standalone-auth.json start > /tmp/mo-result.json
     cat /tmp/mo-result.json | tail -n 1 | php -r 'echo json_decode(file_get_contents("php://stdin"))->mongodb_auth_uri;' > /tmp/uri.txt
     ;;
   REPLICASET)
-    ${TRAVIS_BUILD_DIR}/.travis/mo.sh ${TRAVIS_BUILD_DIR}/.travis/configurations/replica_sets/replicaset.json start > /tmp/mo-result.json
+    ${BUILD_DIR}/.build/mo.sh ${BUILD_DIR}/.build/configurations/replica_sets/replicaset.json start > /tmp/mo-result.json
     cat /tmp/mo-result.json | tail -n 1 | php -r 'echo json_decode(file_get_contents("php://stdin"))->mongodb_uri;' > /tmp/uri.txt
     ;;
   *)
-    ${TRAVIS_BUILD_DIR}/.travis/mo.sh ${TRAVIS_BUILD_DIR}/.travis/configurations/standalone/standalone.json start > /tmp/mo-result.json
+    ${BUILD_DIR}/.build/mo.sh ${BUILD_DIR}/.build/configurations/standalone/standalone.json start > /tmp/mo-result.json
     cat /tmp/mo-result.json | tail -n 1 | php -r 'echo json_decode(file_get_contents("php://stdin"))->mongodb_uri;' > /tmp/uri.txt
     ;;
 esac
