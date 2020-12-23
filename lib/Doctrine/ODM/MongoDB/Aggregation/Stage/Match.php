547b6cbbe2a4eca651573e5a78fa5903556f7060
Andreas Braun <git@alcaeus.org>
2020-12-23 11:23:13 +0100

    Improve deprecations of Match classes

--- lib/Doctrine/ODM/MongoDB/Aggregation/Stage/Match.php
+++ lib/Doctrine/ODM/MongoDB/Aggregation/Stage/Match.php
@@ -4,9 +4,24 @@ declare(strict_types=1);
 
 namespace Doctrine\ODM\MongoDB\Aggregation\Stage;
 
+use Doctrine\ODM\MongoDB\Aggregation\Builder;
+use function trigger_deprecation;
+
 /**
- * @deprecated Deprecated in favour of the MatchStage class
+ * @deprecated This class was deprecated in doctrine/mongodb-odm 2.2. Please use the MatchStage class instead
  */
 class Match extends MatchStage
 {
+    public function __construct(Builder $builder)
+    {
+        trigger_deprecation(
+            'doctrine/mongodb-odm',
+            '2.2',
+            'The "%s" class is deprecated. Please use "%s" instead.',
+            self::class,
+            MatchStage::class
+        );
+
+        parent::__construct($builder);
+    }
 }
