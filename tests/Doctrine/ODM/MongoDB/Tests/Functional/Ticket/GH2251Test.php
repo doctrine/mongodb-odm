498478488c7d8539239e697b2260b4944b2d7b7a
Andreas Braun <alcaeus@users.noreply.github.com>
2021-04-13 11:15:29 +0200

    Fix preparation of $elemMatch operators in queries (#2298)

---
+++ tests/Doctrine/ODM/MongoDB/Tests/Functional/Ticket/GH2251Test.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Doctrine\ODM\MongoDB\Tests\Functional\Ticket;
+
+use Doctrine\ODM\MongoDB\Tests\BaseTest;
+use Documents\User;
+use MongoDB\BSON\ObjectId;
+
+class GH2251Test extends BaseTest
+{
+    /**
+     * @testWith ["groups"]
+     *           ["groupsSimple"]
+     */
+    public function testElemMatchQuery(string $fieldName)
+    {
+        $builder = $this->dm->createQueryBuilder(User::class);
+
+        $objectIds = [
+            new ObjectId('5fae9a775ef4492e3c72b3f3'),
+            new ObjectId('5fae9a775ef4492e3c72b3f4'),
+        ];
+
+        $notIn     = $builder->expr()->notIn($objectIds);
+        $elemMatch = $builder->expr()
+            ->field($fieldName)
+            ->elemMatch($notIn);
+
+        $builder->addNor(
+            $elemMatch
+        );
+
+        $this->assertSame(
+            [
+                '$nor' => [
+                    [
+                        $fieldName => [
+                            '$elemMatch' => ['$nin' => $objectIds],
+                        ],
+                    ],
+                ],
+            ],
+            $builder->getQueryArray()
+        );
+    }
+}
