4285401f4b16cbe6f6fa2dc03211abd00065aaf3
Vincent Le Henaff <v.le-henaff@webmarketing-booster.com>
2021-07-18 02:07:37 +0200

    + add exprOp test + add contextual missing type hint for exprOr

--- tests/Doctrine/ODM/MongoDB/Tests/Aggregation/AggregationOperatorsProviderTrait.php
+++ tests/Doctrine/ODM/MongoDB/Tests/Aggregation/AggregationOperatorsProviderTrait.php
@@ -194,6 +194,11 @@ trait AggregationOperatorsProviderTrait
                 'operator' => 'exp',
                 'args' => ['$field'],
             ],
+            'exprOp' => [
+                'expected' => ['$expr' => '$array'],
+                'operator' => 'expr',
+                'args' => ['$array'],
+            ],
             'filter' => [
                 'expected' => ['$filter' => ['input' => '$array', 'as' => '$as', 'cond' => '$cond']],
                 'operator' => 'filter',
