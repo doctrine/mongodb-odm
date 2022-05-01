bf2ca447e8d626573a9464393bfb48352fac63b1
Fran Moreno <franmomu@gmail.com>
2022-05-01 07:34:47 +0200

    Leverage generic persistence event classes

--- lib/Doctrine/ODM/MongoDB/Event/LoadClassMetadataEventArgs.php
+++ lib/Doctrine/ODM/MongoDB/Event/LoadClassMetadataEventArgs.php
@@ -5,20 +5,18 @@ declare(strict_types=1);
 namespace Doctrine\ODM\MongoDB\Event;
 
 use Doctrine\ODM\MongoDB\DocumentManager;
+use Doctrine\ODM\MongoDB\Mapping\ClassMetadata;
 use Doctrine\Persistence\Event\LoadClassMetadataEventArgs as BaseLoadClassMetadataEventArgs;
-
-use function assert;
 
 /**
  * Class that holds event arguments for a loadMetadata event.
+ *
+ * @template-extends BaseLoadClassMetadataEventArgs<ClassMetadata<object>, DocumentManager>
  */
 final class LoadClassMetadataEventArgs extends BaseLoadClassMetadataEventArgs
 {
     public function getDocumentManager(): DocumentManager
     {
-        $dm = $this->getObjectManager();
-        assert($dm instanceof DocumentManager);
-
-        return $dm;
+        return $this->getObjectManager();
     }
 }
--- lib/Doctrine/ODM/MongoDB/Event/ManagerEventArgs.php
+++ lib/Doctrine/ODM/MongoDB/Event/ManagerEventArgs.php
@@ -7,18 +7,15 @@ namespace Doctrine\ODM\MongoDB\Event;
 use Doctrine\ODM\MongoDB\DocumentManager;
 use Doctrine\Persistence\Event\ManagerEventArgs as BaseManagerEventArgs;
 
-use function assert;
-
 /**
  * Provides event arguments for the flush events.
+ *
+ * @template-extends BaseManagerEventArgs<DocumentManager>
  */
 class ManagerEventArgs extends BaseManagerEventArgs
 {
     public function getDocumentManager(): DocumentManager
     {
-        $dm = $this->getObjectManager();
-        assert($dm instanceof DocumentManager);
-
-        return $dm;
+        return $this->getObjectManager();
     }
 }
