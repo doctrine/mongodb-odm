5c82e9b439944e3c218032b9ae7dfa6be62455cf
Fran Moreno <franmomu@gmail.com>
2022-07-17 23:26:10 +0200

    Fix failing tests
    This tests started to fail with the 1.14 version of the driver

--- tests/Documents/SchemaValidated.php
+++ tests/Documents/SchemaValidated.php
@@ -30,7 +30,7 @@ class SchemaValidated
     },
     "$or": [
         { "phone": { "$type": "string" } },
-        { "email": { "$regex": { "$regularExpression" : { "pattern": "@mongodb\\.com$", "options": "" } } } },
+        { "email": { "$regularExpression" : { "pattern": "@mongodb\\.com$", "options": "" } } },
         { "status": { "$in": [ "Unknown", "Incomplete" ] } }
     ]
 }
