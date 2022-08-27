605e68f4189f0bbfd42e0de055a65e0dbceeacf9
Vincent Langlet <VincentLanglet@users.noreply.github.com>
2022-08-27 15:53:28 +0200

    Make Iterator covariant

--- lib/Doctrine/ODM/MongoDB/Iterator/Iterator.php
+++ lib/Doctrine/ODM/MongoDB/Iterator/Iterator.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Doctrine\ODM\MongoDB\Iterator;
 
 /**
- * @template TValue
+ * @template-covariant TValue
  * @template-extends \Iterator<mixed, TValue>
  */
 interface Iterator extends \Iterator
