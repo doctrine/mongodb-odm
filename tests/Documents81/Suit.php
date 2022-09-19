307180ad10b0299d800f85fb2babe8c0623715a3
Maciej Malarz <malarzm@gmail.com>
2022-09-19 23:04:26 +0200

    Fix coding standard violations

--- tests/Documents81/Suit.php
+++ tests/Documents81/Suit.php
@@ -6,8 +6,8 @@ namespace Documents81;
 
 enum Suit: string
 {
-    case Hearts = 'H';
+    case Hearts   = 'H';
     case Diamonds = 'D';
-    case Clubs = 'C';
-    case Spades = 'S';
+    case Clubs    = 'C';
+    case Spades   = 'S';
 }
