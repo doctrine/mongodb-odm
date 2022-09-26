5206a889ee9215efaefbacd06163718657b7d714
Dmitry Khaperets <khaperets@gmail.com>
2022-09-26 11:47:36 +0200

    Add pipeline to $lookup stage

--- tests/Doctrine/ODM/MongoDB/Tests/Aggregation/Stage/LookupTest.php
+++ tests/Doctrine/ODM/MongoDB/Tests/Aggregation/Stage/LookupTest.php
@@ -71,8 +71,6 @@ class LookupTest extends BaseTest
                 '$lookup' => [
                     'from' => 'users',
                     'as' => 'user',
-                    'localField' => 'userId',
-                    'foreignField' => '_id',
                     'let' => ['name' => '$username'],
                     'pipeline' => [
                         [
