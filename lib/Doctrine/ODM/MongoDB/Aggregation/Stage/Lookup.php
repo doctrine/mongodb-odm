7a155184e6a0564d3e04adf12f6751eee66cbc0f
Ali Turki <aturki@users.noreply.github.com>
2023-02-15 08:59:37 +0100

    Allow using aggregation builder in lookup stage pipeline (#2486)
    * use builder in lookup stage pipeline
    
    * Avoid using the same builder for pipeline lookup
    
    ---------
    
    Co-authored-by: Fran Moreno <franmomu@gmail.com>

--- lib/Doctrine/ODM/MongoDB/Aggregation/Stage/Lookup.php
+++ lib/Doctrine/ODM/MongoDB/Aggregation/Stage/Lookup.php
@@ -11,6 +11,7 @@ use Doctrine\ODM\MongoDB\Mapping\ClassMetadata;
 use Doctrine\ODM\MongoDB\Mapping\MappingException;
 use Doctrine\ODM\MongoDB\Persisters\DocumentPersister;
 use Doctrine\Persistence\Mapping\MappingException as BaseMappingException;
+use InvalidArgumentException;
 
 /**
  * Fluent interface for building aggregation pipelines.
@@ -34,8 +35,8 @@ class Lookup extends Stage
     /** @var array<string, string>|null */
     private ?array $let = null;
 
-    /** @var array<array<string, mixed>>|null */
-    private ?array $pipeline = null;
+    /** @var Builder|array<array<string, mixed>>|null */
+    private $pipeline = null;
 
     private bool $excludeLocalAndForeignField = false;
 
@@ -109,8 +110,12 @@ class Lookup extends Stage
             $expression['$lookup']['let'] = $this->let;
         }
 
-        if (! empty($this->pipeline)) {
-            $expression['$lookup']['pipeline'] = $this->pipeline;
+        if ($this->pipeline !== null) {
+            if ($this->pipeline instanceof Builder) {
+                $expression['$lookup']['pipeline'] = $this->pipeline->getPipeline(false);
+            } else {
+                $expression['$lookup']['pipeline'] = $this->pipeline;
+            }
         }
 
         return $expression;
@@ -171,11 +176,19 @@ class Lookup extends Stage
      * Instead, define variables for the joined document fields using the let option
      * and then reference the variables in the pipeline stages.
      *
-     * @param array<array<string, mixed>> $pipeline
+     * @param Builder|Stage|array<array<string, mixed>> $pipeline
      */
-    public function pipeline(array $pipeline): self
+    public function pipeline($pipeline): self
     {
-        $this->pipeline = $pipeline;
+        if ($pipeline instanceof Stage) {
+            $this->pipeline = $pipeline->builder;
+        } else {
+            $this->pipeline = $pipeline;
+        }
+
+        if ($this->builder === $this->pipeline) {
+            throw new InvalidArgumentException('Cannot use the same Builder instance for $lookup pipeline.');
+        }
 
         return $this;
     }
