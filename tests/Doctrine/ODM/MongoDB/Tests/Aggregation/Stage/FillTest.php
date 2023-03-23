742d27c2e4189de936ee297c263231d34f14d9e1
Andreas Braun <git@alcaeus.org>
2023-03-23 09:16:09 +0100

    Test complex values and sort for $fill

--- tests/Doctrine/ODM/MongoDB/Tests/Aggregation/Stage/FillTest.php
+++ tests/Doctrine/ODM/MongoDB/Tests/Aggregation/Stage/FillTest.php
@@ -14,14 +14,18 @@ class FillTest extends BaseTest
 
     public function testStage(): void
     {
-        $fillStage = new Fill($this->getTestAggregationBuilder());
+        $builder   = $this->getTestAggregationBuilder();
+        $fillStage = new Fill($builder);
         $fillStage
             ->partitionByFields('field1', 'field2')
             ->sortBy('field1', 1)
             ->output()
                 ->field('foo')->locf()
                 ->field('bar')->linear()
-                ->field('fixed')->value(0);
+                ->field('fixed')->value(0)
+                ->field('computed')->value(
+                    $builder->expr()->multiply('$value', 5),
+                );
 
         self::assertEquals(
             [
@@ -32,6 +36,33 @@ class FillTest extends BaseTest
                         'foo' => ['method' => 'locf'],
                         'bar' => ['method' => 'linear'],
                         'fixed' => ['value' => 0],
+                        'computed' => ['value' => ['$multiply' => ['$value', 5]]],
+                    ],
+                ],
+            ],
+            $fillStage->getExpression(),
+        );
+    }
+
+    public function testStageWithComplexSort(): void
+    {
+        $fillStage = new Fill($this->getTestAggregationBuilder());
+        $fillStage
+            ->partitionByFields('field1', 'field2')
+            ->sortBy(['field1' => 'asc', 'field2' => 'desc'])
+            ->output()
+                ->field('foo')->locf();
+
+        self::assertEquals(
+            [
+                '$fill' => (object) [
+                    'partitionByFields' => ['field1', 'field2'],
+                    'sortBy' => (object) [
+                        'field1' => 1,
+                        'field2' => -1,
+                    ],
+                    'output' => (object) [
+                        'foo' => ['method' => 'locf'],
                     ],
                 ],
             ],
