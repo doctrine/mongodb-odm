651028ca4e0b497127780e3deb66826b54023dd9
Patrick Reimers <preimers@pm.me>
2023-12-09 14:21:34 +0100

    add test creating a time series collection

---
+++ tests/Doctrine/ODM/MongoDB/Tests/Functional/TimeseriesCollectionsTest.php
@@ -0,0 +1,76 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Doctrine\ODM\MongoDB\Tests\Functional;
+
+use DateTime;
+use DateTimeInterface;
+use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
+use Doctrine\ODM\MongoDB\Tests\BaseTestCase;
+
+class TimeseriesCollectionsTest extends BaseTestCase
+{
+    public function testCreateTimeseriesCollectionsCapped(): void
+    {
+        $sm = $this->dm->getSchemaManager();
+        $sm->dropDocumentCollection(TimeseriesCollectionTestDocument::class);
+        $sm->createDocumentCollection(TimeseriesCollectionTestDocument::class);
+
+        $coll                = $this->dm->getDocumentCollection(TimeseriesCollectionTestDocument::class);
+        $document            = new TimeseriesCollectionTestDocument();
+        $document->createdAt = new DateTime('2023-12-09 00:00:00');
+        $document->metaData  = ['foo' => 'bar'];
+        $document->value     = 1337;
+        $this->dm->persist($document);
+        $this->dm->flush();
+
+        $data = $coll->find()->toArray();
+        self::assertCount(1, $data);
+    }
+}
+
+/**
+ * @ODM\Document(collection={
+ *   "name"="TimeseriesCollectionTest",
+ *   "capped"=true,
+ *   "size"=1000,
+ *   "max"=1,
+ *   "timeseries"={
+ *     "timeField"="createdAt",
+ *     "metaField"="metaData",
+ *     "granularity"="seconds",
+ *  },
+ *     "expireAfterSeconds"=60
+ * })
+ */
+class TimeseriesCollectionTestDocument
+{
+    /**
+     * @ODM\Id
+     *
+     * @var string|null
+     */
+    public $id;
+
+    /**
+     * @ODM\Field(type="date")
+     *
+     * @var DateTimeInterface|null
+     */
+    public $createdAt;
+
+    /**
+     * @ODM\Field(type="hash")
+     *
+     * @var array|null
+     */
+    public $metaData;
+
+    /**
+     * @ODM\Field(type="integer")
+     *
+     * @var int|null
+     */
+    public $value;
+}
