383b66df27092e64c9400fcf682042da25f4dcf2
Andreas Braun <git@alcaeus.org>
2024-11-28 10:17:57 +0100

    Add joint iterable interface for queries and aggregations

--- lib/Doctrine/ODM/MongoDB/Aggregation/Aggregation.php
+++ lib/Doctrine/ODM/MongoDB/Aggregation/Aggregation.php
@@ -7,11 +7,11 @@ namespace Doctrine\ODM\MongoDB\Aggregation;
 use Doctrine\ODM\MongoDB\DocumentManager;
 use Doctrine\ODM\MongoDB\Iterator\CachingIterator;
 use Doctrine\ODM\MongoDB\Iterator\HydratingIterator;
+use Doctrine\ODM\MongoDB\Iterator\IterableResult;
 use Doctrine\ODM\MongoDB\Iterator\Iterator;
 use Doctrine\ODM\MongoDB\Iterator\UnrewindableIterator;
 use Doctrine\ODM\MongoDB\Mapping\ClassMetadata;
 use Iterator as SPLIterator;
-use IteratorAggregate;
 use MongoDB\Collection;
 use MongoDB\Driver\CursorInterface;
 
@@ -19,7 +19,7 @@ use function array_merge;
 use function assert;
 
 /** @phpstan-import-type PipelineExpression from Builder */
-final class Aggregation implements IteratorAggregate
+final class Aggregation implements IterableResult
 {
     /**
      * @param array<string, mixed> $pipeline
@@ -28,6 +28,26 @@ final class Aggregation implements IteratorAggregate
      */
     public function __construct(private DocumentManager $dm, private ?ClassMetadata $classMetadata, private Collection $collection, private array $pipeline, private array $options = [], private bool $rewindable = true)
     {
+    }
+
+    public function execute(): Iterator
+    {
+        return $this->getIterator();
+    }
+
+    /**
+     * Execute the query and return the first result.
+     *
+     * @return array<string, mixed>|object|null
+     */
+    public function getSingleResult(): mixed
+    {
+        $clone = clone $this;
+
+        // Limit the pipeline to a single result for efficiency
+        $this->pipeline[] = ['$limit' => 1];
+
+        return $clone->getIterator()->current() ?: null;
     }
 
     public function getIterator(): Iterator
