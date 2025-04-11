2005edf7b6e9b66383f58869894ea0c0da60b7e8
Richard Deloge <richarddeloge@gmail.com>
2025-04-11 10:35:15 +0200

    Fix HydratorFactory about nullable date field

--- lib/Doctrine/ODM/MongoDB/Hydrator/HydratorFactory.php
+++ lib/Doctrine/ODM/MongoDB/Hydrator/HydratorFactory.php
@@ -185,10 +185,13 @@ EOF
                     <<<'EOF'
 
         // Field(type: "date")
-        if (isset($data['%1$s'])) {
+        if (isset($data['%1$s']) || (! empty($this->class->fieldMappings['%2$s']['nullable']) && array_key_exists('%1$s', $data))) {
             $value = $data['%1$s'];
             %3$s
-            $this->class->reflFields['%2$s']->setValue($document, clone $return);
+            if (\is_object($return)) {
+              $return = clone $return;
+            }
+            $this->class->reflFields['%2$s']->setValue($document, $return);
             $hydratedData['%2$s'] = $return;
         }
 
