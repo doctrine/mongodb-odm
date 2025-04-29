3afc991bd5960f9c2c5ea12707065d6e72e477ed
Jérôme Tamarelle <jerome@tamarelle.net>
2025-04-29 14:44:58 +0200

    Make Expr constructor final, as it is instanciated with new static

--- lib/Doctrine/ODM/MongoDB/Aggregation/Expr.php
+++ lib/Doctrine/ODM/MongoDB/Aggregation/Expr.php
@@ -78,6 +78,7 @@ class Expr implements
     /** @var array{case: mixed|self, then?: mixed|self}|null */
     private ?array $switchBranch = null;
 
+    /** @final This constructor is used in {@see self::expr()} */
     public function __construct(private DocumentManager $dm, private ClassMetadata $class)
     {
     }
