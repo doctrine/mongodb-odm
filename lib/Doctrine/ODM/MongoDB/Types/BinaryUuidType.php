4e658da1b609431f9417a5d712be3cae02d9bd5d
Andreas Braun <git@alcaeus.org>
2025-09-23 09:56:39 +0200

    Unify logic in binary uuid type

--- lib/Doctrine/ODM/MongoDB/Types/BinaryUuidType.php
+++ lib/Doctrine/ODM/MongoDB/Types/BinaryUuidType.php
@@ -5,29 +5,25 @@ declare(strict_types=1);
 namespace Doctrine\ODM\MongoDB\Types;
 
 use Exception;
+use InvalidArgumentException;
 use MongoDB\BSON\Binary;
 use Symfony\Component\Uid\Uuid;
 
 use function get_debug_type;
+use function is_string;
 use function sprintf;
 
 class BinaryUuidType extends Type
 {
     public function convertToDatabaseValue(mixed $value): ?Binary
     {
-        if ($value === null) {
-            return null;
-        }
-
-        if ($value instanceof Binary) {
-            return $value;
-        }
-
-        if (! $value instanceof Uuid) {
-            $value = Uuid::fromString($value);
-        }
-
-        return new Binary($value->toBinary(), Binary::TYPE_UUID);
+        return match (true) {
+            $value === null => null,
+            $value instanceof Binary => $value,
+            $value instanceof Uuid => new Binary($value->toBinary(), Binary::TYPE_UUID),
+            is_string($value) => new Binary(Uuid::fromString($value)->toBinary(), Binary::TYPE_UUID),
+            default => throw new InvalidArgumentException(sprintf('Invalid data type %s received for UUID', get_debug_type($value))),
+        };
     }
 
     public function convertToPHPValue(mixed $value): Uuid
@@ -55,13 +51,28 @@ $return = match (true) {
     $value instanceof \MongoDB\BSON\Binary => $value,
     $value instanceof \Symfony\Component\Uid\Uuid => new \MongoDB\BSON\Binary($value->toBinary(), \MongoDB\BSON\Binary::TYPE_UUID),
     is_string($value) => new \MongoDB\BSON\Binary(\Symfony\Component\Uid\Uuid::fromString($value)->toBinary(), \MongoDB\BSON\Binary::TYPE_UUID),
-    default => throw new InvalidArgumentException(sprintf('Invalid data type %s received for UUID', get_debug_type($value))),
+    default => throw new \InvalidArgumentException(sprintf('Invalid data type %s received for UUID', get_debug_type($value))),
 };
 PHP;
     }
 
     public function closureToPHP(): string
     {
-        return '$return = $value instanceof \Symfony\Component\Uid\Uuid ? $value : \Symfony\Component\Uid\Uuid::fromBinary($value->getData());';
+        return <<<'PHP'
+        if ($value instanceof \Symfony\Component\Uid\Uuid) {
+            $return = $value;
+            return;
+        }
+
+        if (! $value instanceof \MongoDB\BSON\Binary) {
+            throw new \InvalidArgumentException(sprintf('Invalid data of type "%s" received for Uuid', get_debug_type($value)));
+        }
+
+        if ($value->getType() !== \MongoDB\BSON\Binary::TYPE_UUID) {
+            throw new \InvalidArgumentException(sprintf('Invalid binary data of type %d received for Uuid', $value->getType()));
+        }
+
+        $return = \Symfony\Component\Uid\Uuid::fromBinary($value->getData());
+PHP;
     }
 }
