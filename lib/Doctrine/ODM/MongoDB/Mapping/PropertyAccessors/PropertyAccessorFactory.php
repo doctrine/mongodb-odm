81897149be49edb29b785aef4bb2704a3c4c0848
Jérôme Tamarelle <jerome@tamarelle.net>
2025-09-27 00:17:14 +0200

    Import all PropertyAccessor classes from ORM
    https://github.com/doctrine/orm/blob/4.0.x/src/Mapping/PropertyAccessors/EmbeddablePropertyAccessor.php

---
+++ lib/Doctrine/ODM/MongoDB/Mapping/PropertyAccessors/PropertyAccessorFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Doctrine\ODM\MongoDB\Mapping\PropertyAccessors;
+
+use ReflectionProperty;
+
+class PropertyAccessorFactory
+{
+    /** @phpstan-param class-string $className */
+    public static function createPropertyAccessor(string $className, string $propertyName): PropertyAccessor
+    {
+        $reflectionProperty = new ReflectionProperty($className, $propertyName);
+
+        $accessor = RawValuePropertyAccessor::fromReflectionProperty($reflectionProperty);
+
+        if ($reflectionProperty->hasType() && ! $reflectionProperty->getType()->allowsNull()) {
+            $accessor = new TypedNoDefaultPropertyAccessor($accessor, $reflectionProperty);
+        }
+
+        if ($reflectionProperty->isReadOnly()) {
+            $accessor = new ReadonlyAccessor($accessor, $reflectionProperty);
+        }
+
+        return $accessor;
+    }
+}
