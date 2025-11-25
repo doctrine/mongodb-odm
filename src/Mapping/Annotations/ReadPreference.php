7b0bd3582004e30e5d59fc277471781adf3d5b05
Jérôme Tamarelle <jerome@tamarelle.net>
2025-11-25 12:27:25 +0100

    Add deprecated aliases for attribute classes in Annotations namespace

---
+++ src/Mapping/Annotations/ReadPreference.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Doctrine\ODM\MongoDB\Mapping\Annotations;
+
+use Doctrine\ODM\MongoDB\Mapping\Attribute\ReadPreference;
+
+use function class_exists;
+use function trigger_deprecation;
+
+trigger_deprecation('doctrine/mongodb-odm', '2.16', 'Namespace "Doctrine\ODM\MongoDB\Mapping\Annotations" is deprecated, use "Doctrine\ODM\MongoDB\Mapping\Attribute" instead.');
+
+class_exists(ReadPreference::class);
